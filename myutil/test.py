cf74c9ed1426c5335f19e0cb9baeb93e2142dff4
Kev. Noel <arita37@users.noreply.github.com>
2021-04-02 14:45:08 +0900

    Update test.py

--- myutil/test.py
+++ myutil/test.py
@@ -1,15 +1,21 @@
 import os, sys, time, datetime,inspect
 
 
-
 ##################################################################################################
 def test1():
    from myutil import os_makedirs, Session, verbosity_get
+ 
    os_makedirs('ztmp/ztmp2/myfile.txt')
    os_makedirs('ztmp/ztmp3/ztmp4')
+   os.system("ls ztmp")
 
 
+   print('verbo', verbosity_get(__file__, "../confi.json", 40,))
    print("success")
+
+   sess = Session("ztmp/session")
+   sess.save('mysess', globals(), '01')
+   os.system("ls ztmp/session")
 
 
 
@@ -21,5 +27,3 @@ if __name__ == "__main__":
 
 
 
-
-      
