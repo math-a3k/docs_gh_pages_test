a5df0dc45fe0bd2ed544501783e0a11f502b28e4
Kev. Noel <arita37@users.noreply.github.com>
2021-04-02 14:36:39 +0900

    Update test.py

--- myutil/test.py
+++ myutil/test.py
@@ -3,7 +3,7 @@ import os, sys, time, datetime,inspect
 
 
 ##################################################################################################
-def test():
+def test1():
    from myutil import os_makedirs, Session, verbosity_get
    os_makedirs('ztmp/ztmp2/myfile.txt')
    os_makedirs('ztmp/ztmp3/ztmp4')
@@ -15,7 +15,7 @@ def test():
 
 if __name__ == "__main__":
     import fire
-    fire.Fire(test)
+    fire.Fire()
 
 
 
