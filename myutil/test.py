2f4aa9f1750016b9395abd9bc5ecdb23b07a74db
Kev. Noel <arita37@users.noreply.github.com>
2021-04-02 13:57:26 +0900

    Create test.py

---
+++ myutil/test.py
@@ -0,0 +1,24 @@
+import os, sys, time, datetime,inspect, json
+
+
+from myutil import verbosity_get
+
+##################################################################################################
+def test():
+   pass
+
+
+  
+  
+  
+  
+if __name__ == "__main__":
+    import fire
+    fire.Fire()
+
+
+
+
+
+
+      
