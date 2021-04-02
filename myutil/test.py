dff13f379062abe58879a70d1f576a73e621cad9
Kev. Noel <arita37@users.noreply.github.com>
2021-04-02 14:27:05 +0900

    Update test.py

--- myutil/test.py
+++ myutil/test.py
@@ -1,9 +1,10 @@
 import os, sys, time, datetime,inspect
 
-from myutil import os_makedirs, Session, verbosity_get
+
 
 ##################################################################################################
 def test():
+   from myutil import os_makedirs, Session, verbosity_get
    os_makedirs('ztmp/ztmp2/myfile.txt')
    os_makedirs('ztmp/ztmp3/ztmp4')
 
