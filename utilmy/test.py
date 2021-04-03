789843f624fe8ae9e754e3fcdf8ecb575232c351
arita37 <noelkev0@gmail.com>
2021-04-03 11:17:30 +0900

    utilmy/test.py

--- utilmy/test.py
+++ utilmy/test.py
@@ -7,20 +7,26 @@ def test1():
                        
                       )
  
+
    os_makedirs('ztmp/ztmp2/myfile.txt')
    os_makedirs('ztmp/ztmp3/ztmp4')
    os.system("ls ztmp")
 
 
-   print('verbo', verbosity_get(__file__, "../confi.json", 40,))
-   print("success")
+   print('verbosity', verbosity_get(__file__, "../confi.json", 40,))
+
 
    sess = Session("ztmp/session")
    sess.save('mysess', globals(), '01')
    os.system("ls ztmp/session")
 
+
    res = os_system( f" ls . ",  doprint=True) 
    print(res)
+
+
+   print("success")
+
 
 if __name__ == "__main__":
     import fire
