1bc92631f56dee3922d88f56316559d98490522f
arita37 <noelkev0@gmail.com>
2021-04-03 11:08:31 +0900

    run_pipy.py

--- setup.py
+++ setup.py
@@ -1,26 +1,127 @@
-from setuptools import setup, find_packages
+# pylint: disable=C0321,C0103,C0301,E1305,E1121,C0302,C0330,C0111,W0613,W0611,R1705
+# -*- coding: utf-8 -*-
+"""
+
+
+"""
+import io, os, subprocess, sys
+from setuptools import find_packages, setup
+
+######################################################################################
+root = os.path.abspath(os.path.dirname(__file__))
+
+
+
+##### Version  #######################################################################
+version ='0.0.3'
+cmdclass= None
+print("version", version)
+
+
+
+##### Requirements ###################################################################
+#with open('install/requirements.txt') as fp:
+#    install_requires = fp.read()
+install_requires = []
+
+
+
+###### Description ###################################################################
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+long_description =  """
+
+```
+Utils
+
+
+```
+"""
+
+
+
+### Packages  ########################################################
+packages = ["utilmy"] + ["utilmy." + p for p in find_packages("utilmy")]
+print(packages)
+
+
+scripts = [     ]
+
+
+
+### CLI Scripts  ###################################################   
+entry_points={ 'console_scripts': [
+
+              ] }
+
+
+
+
+
+
+##################################################################   
 setup(
-    name='utilmy',
-    version='0.0.3',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts':
-            [
+    name="utilmy",
+    description="utils",
+    keywords='utils',
+    
+    author="Nono",    
+    install_requires=install_requires,
+    python_requires='>=3.6.5',
+    
+    packages=packages,
 
+    include_package_data=True,
+    #    package_data= {'': extra_files},
 
-            ],
+    package_data={
+       '': ['*','*/*','*/*/*','*/*/*/*']
     },
-    license='MIT',
-    description='utilities for python scripts.',
-    keywords=['PYTHON', 'CLI', 'UTILITIES'],
-    install_requires=[
-    ],
+
+   
+    ### Versioning
+    version=version,
+    #cmdclass=cmdclass,
+
+
+    #### CLI
+    scripts = scripts,
+  
+    ### CLI pyton
+    entry_points= entry_points,
+
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Education',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'
-    ],
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: ' +
+          'Artificial Intelligence',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: ' +
+          'Python Modules',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS :: MacOS X',
+      ]
 )
+
+
+
+
+
+
+
 
