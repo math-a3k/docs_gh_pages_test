d80ad93b6dfa090f5ba65be9810b69a1ad01df86
kaushik sathvara <kaushiksathvara74@gmail.com>
2021-05-28 07:53:39 +0530

    updated argument as a list

--- test.py
+++ test.py
@@ -201,7 +201,7 @@ def test3():
 
     cols = ['name','pet_name']
     sample_df = pd.DataFrame(zip(list1, list2), columns=cols)
-    original_value = text.pd_similarity(sample_df, *cols)['score']
+    original_value = text.pd_similarity(sample_df, cols)['score']
 
     check_similarity = lambda *x: SequenceMatcher(None, *x[0]).ratio()
     
--- utilmy/text.py
+++ utilmy/text.py
@@ -7,8 +7,12 @@ https://pypi.org/project/pysie/#description
 
 
 """
-import os, sys, pandas as pd, numpy as np
+import os
+import sys
+import pandas as pd
+import numpy as np
 from typing import List
+
 
 def log(*s):
     print(s)
@@ -22,104 +26,106 @@ def pd_text_hash_create_lsh(df, col, sep=" ", threshold=0.7, num_perm=10):
     For each of the entry create a hash function
     '''
     from datasketch import MinHash, MinHashLSH
-    #Create LSH
+    # Create LSH
     lsh = MinHashLSH(threshold=threshold, num_perm=num_perm)
 
-    #Intialize list
+    # Intialize list
     hash_lines = []
 
     ll = df[col].values
     for index, sentence in enumerate(ll):
 
-        #Get tokens of individual elements
+        # Get tokens of individual elements
         tokens = sentence.split(sep)
 
-        #Create local hash funtion
+        # Create local hash funtion
         v = MinHash(num_perm=num_perm)
 
         for j in set(tokens):
             v.update(j.encode('utf8'))
 
-        #Append
+        # Append
         hash_lines.append(v)
         lsh.insert(str(index), v)
     return hash_lines, lsh
-
 
 
 def pd_text_getcluster(df, col, threshold, num_perm):
     '''
     For each of the hash function find a cluster and assign unique id to the dataframe cluster_id
     '''
-    #MAster cluster ids
+    # MAster cluster ids
     all_cluster_ids = []
 
-    #REturn from hash list
-    hash_lines, lsh = pd_text_hash_create_lsh(df, col=col, threshold = threshold, num_perm = num_perm)
+    # REturn from hash list
+    hash_lines, lsh = pd_text_hash_create_lsh(
+        df, col=col, threshold=threshold, num_perm=num_perm)
 
-    #For each local hash find the cluster ids and assign to the dataframe and return dataframe
+    # For each local hash find the cluster ids and assign to the dataframe and return dataframe
     cluster_count = 1
     for ind, i in enumerate(hash_lines):
 
         if ind in all_cluster_ids:
             continue
 
-        x_duplicate     = lsh.query(i)
+        x_duplicate = lsh.query(i)
         x_duplicate_int = list(map(int, x_duplicate))
-        #print(x_duplicate_int)
+        # print(x_duplicate_int)
         df.at[x_duplicate_int, 'cluster_id'] = cluster_count
-        cluster_count   += 1
+        cluster_count += 1
         all_cluster_ids += x_duplicate_int
 
     return df
 
 
-def pd_similarity(df: pd.DataFrame, cols: List[str], algo='')->pd.DataFrame:
+def pd_similarity(df: pd.DataFrame, cols=[], algo='') -> pd.DataFrame:
     '''
         Return similarities between two columns with 
         python's SequenceMatcher algorithm
-        
+
         Args:
             df (pd.DataFrame): Pandas Dataframe.
             algo (String)    : rapidfuzz | editdistance 
-            cols (list[str]) : List of of columns name
-        
+            cols (list[str]) : List of of columns name (2 columns)
+
         Returns:
             pd.DataFrame
-        
-    '''
 
+    '''
+    if len(cols) != 2:
+        raise Exception("Add two columns")
     for col in cols:
         if col not in df:
             raise Exception(f"Columns not found {col}")
             break
-    
+
     from difflib import SequenceMatcher
-    from rapidfuzz import fuzz            
+    from rapidfuzz import fuzz
     import editdistance
-    
-    def find_similarity(*values):
-        if algo=="rapidfuzz":
-            similarity_score = fuzz.ratio(*values[0])
-        elif algo=="editdistance":
-            similarity_score = editdistance.eval(*values[0])
+
+    def find_similarity(col1, col2):
+        if algo == "rapidfuzz":
+            similarity_score = fuzz.ratio(col1, col2)
+        elif algo == "editdistance":
+            similarity_score = editdistance.eval(col1, col2)
         else:
             is_junk = None
-            similarity_score = SequenceMatcher(is_junk, *values[0]).ratio()
+            similarity_score = SequenceMatcher(is_junk, col1, col2).ratio()
         return similarity_score
-    df['score'] = df[cols].apply( lambda x : find_similarity(x[cols]), axis=1)
+    df['score'] = df.apply(lambda x: find_similarity(
+        x[cols[0]], x[cols[1]]), axis=1)
     return df
+
 
 def test_lsh():
 
-    ll = [ 'aa bb cc', 'a b c', 'cc bb cc']
+    ll = ['aa bb cc', 'a b c', 'cc bb cc']
     column_name = "sentence"
-    threshold   = 0.7
-    num_perm    = 10
-    num_items   = 100000
+    threshold = 0.7
+    num_perm = 10
+    num_items = 100000
 
-    df   = pd.DataFrame(ll, columns = [column_name])
-    df1  = pd_text_getcluster(df.head(num_items), column_name, threshold, num_perm)
+    df = pd.DataFrame(ll, columns=[column_name])
+    df1 = pd_text_getcluster(
+        df.head(num_items), column_name, threshold, num_perm)
     print(df1)
-
-
