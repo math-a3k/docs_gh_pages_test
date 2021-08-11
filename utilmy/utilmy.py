d9f60ca98a46c6633e41d256c1e8037933aa3727
Kev. Noel <arita37@users.noreply.github.com>
2021-08-11 10:47:02 +0900

    Update utilmy.py

--- utilmy/utilmy.py
+++ utilmy/utilmy.py
@@ -451,6 +451,21 @@ def to_int(x):
         return float("NaN")
 
 
+def is_int(x):
+    try :
+        int(x)
+        return True
+    except :
+        return False    
+
+def is_float(x):
+    try :
+        float(x)
+        return True
+    except :
+        return False   
+
+
 
 
 ########################################################################################################
