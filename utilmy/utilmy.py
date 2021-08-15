260d5f7966bfed920316370d66cc52bca90eb9e8
Kev. Noel <arita37@users.noreply.github.com>
2021-08-16 02:22:56 +0900

    Update utilmy.py

--- utilmy/utilmy.py
+++ utilmy/utilmy.py
@@ -1064,7 +1064,108 @@ def hdfs_put(from_dir="", to_dir="",  verbose=True, n_pool=25, dirlevel=50,  **k
     pool.terminate() ; pool.join()  ;  pool = None          
     log('n_processed', len(res_list) )
     
- 
+
+
+
+def hdfs_walk(path="hdfs://nameservice1/user/", dirlevel=3, hdfs=None):   ### python  prepro.py hdfs_walk 
+    import pyarrow as pa
+    hdfs = pa.hdfs.connect() if hdfs is None else hdfs
+    path = "hdfs://nameservice1/" + path if 'hdfs://' not in path else path   
+         
+    def os_walk(fdirs):
+        flist3 = []
+        for diri  in fdirs :
+            flist3.extend( [ t for t in hdfs.ls(diri) ]  )
+        fdirs3 = [ t   for t in flist3 if hdfs.isdir(t) ]  
+        return flist3, fdirs3
+    
+    flist0, fdirs0   = os_walk([path])
+    fdirs = fdirs0
+    for i in range(dirlevel):
+       flisti, fdiri = os_walk(fdirs)     
+       flist0 =  list(set(flist0  + flisti ))
+       fdirs0 =  list(set(fdirs0  + fdiri ))
+       fdirs  = fdiri    
+    return {'file': flist0, 'dir': fdirs0}
+    
+    
+    
+def hdfs_get(from_dir="", to_dir="",  verbose=True, n_pool=20,   **kw):
+    """ 
+    import fastcounter
+    counter = fastcounter.FastWriteCounter,()
+    counter.increment(1)    
+    cnt.value
+    """
+    import glob, gc,os, time
+    from multiprocessing.pool import ThreadPool
+
+    def log(*s, **kw):
+      print(*s, flush=True, **kw)
+    
+    #### File ############################################
+    os.makedirs(to_dir, exist_ok=True)    
+    import pyarrow as pa
+    hdfs      = pa.hdfs.connect()  
+    # file_list = [ t for t in hdfs.ls(from_dir) ] 
+    file_list = hdfs_walk(from_dir, dirlevel=10)['file']
+    
+
+    def fun_async(xlist):
+      for x in xlist :   
+         try :
+            hdfs.download(x[0], x[1])
+            # ktot = ktot + 1   ### Not thread safe
+         except :
+            try :
+               time.sleep(60)
+               hdfs.download(x[0], x[1])
+               # ktot = ktot + 1
+            except : pass
+            
+    ######################################################    
+    file_list = sorted(list(set(file_list)))
+    n_file    = len(file_list)
+    log('Files', n_file)
+    if verbose: log(file_list)
+
+    xi_list = [ []  for t in range(n_pool) ]     
+    for i, filei in enumerate(file_list) :
+        jj = i % n_pool 
+        xi_list[jj].append( (filei,   to_dir + "/" + filei.split("/")[-1]   )  )
+        
+    #### Pool count
+    pool     = ThreadPool(processes=n_pool)
+    job_list = []
+    for i in range(n_pool):
+         job_list.append( pool.apply_async(fun_async, (xi_list[i], )))
+         if verbose : log(i, xi_list[i] )
+
+    res_list = []            
+    for i in range(n_pool):
+        if i >= len(job_list): break
+        res_list.append( job_list[ i].get() )
+        log(i, 'job finished')
+
+    pool.terminate()  ;  pool.join()  ; pool = None          
+    log('n_processed', len(res_list) )
+    log('n files', len(os.listdir(to_dir)) )
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
  def multithread_run(fun_async, input_list:list, n_pool=5, verbose=True):
     """  input is as list of tuples
