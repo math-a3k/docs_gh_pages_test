3565ceebde74770a989351ec41ab84313890e0fc
Kev. Noel <arita37@users.noreply.github.com>
2021-08-16 11:01:20 +0900

    Update decorators.py

--- utilmy/decorators.py
+++ utilmy/decorators.py
@@ -1,21 +1,93 @@
 from threading import Thread
 import cProfile, pstats, io
-import os
-import errno
-import signal
+import os, errno, signal, time
 from functools import wraps
-import time
 from contextlib import contextmanager
+
+
+
+
+########################################################################################################################
+########################################################################################################################
+ def multithread_run(fun_async, input_list:list, n_pool=5, verbose=True):
+    """  input is as list of tuples  [(x1,x2,x3), (y1,y2,y3) ]
+    def fun_async(xlist):
+      for x in xlist :   
+            hdfs.upload(x[0], x[1])
+    """
+    #### Input xi #######################################    
+    xi_list = [ []  for t in range(n_pool) ]     
+    for i, xi in enumerate(input_list) :
+        jj = i % n_pool 
+        xi_list[jj].append( xi )
+
+    #### Pool execute ###################################
+    pool     = ThreadPool(processes=n_pool)
+    job_list = []
+    for i in range(n_pool):
+         job_list.append( pool.apply_async(fun_async, (xi_list[i], )))
+         if verbose : log(i, xi_list[i] )
+
+    res_list = []            
+    for i in range(n_pool):
+        if i >= len(job_list): break
+        res_list.append( job_list[ i].get() )
+        log(i, 'job finished')
+
+    pool.terminate() ; pool.join()  ; pool = None          
+    log('n_processed', len(res_list) )    
+
+
+def multithread_run_list(**kwargs):
+    """ Creating n number of threads:  1 thread per function,    starting them and waiting for their subsequent completion
+    os_multithread(function1=(test_print, ("some text",)),
+                          function2=(test_print, ("bbbbb",)),
+                          function3=(test_print, ("ccccc",)))
+    """
+    class ThreadWithResult(Thread):
+        def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None):
+            def function():
+                self.result = target(*args, **kwargs)
+            super().__init__(group=group, target=function, name=name, daemon=daemon)
+
+    list_of_threads = []
+    for thread in kwargs.values():
+        t = ThreadWithResult(target=thread[0], args=thread[1])
+        list_of_threads.append(t)
+
+    for thread in list_of_threads:
+        thread.start()
+
+    results = []
+    for thread, keys in zip(list_of_threads, kwargs.keys()):
+        thread.join()
+        results.append((keys, thread.result))
+
+    return results
+
+
+def thread_deco(func):
+    """ A decorator to run function in background on thread
+	Return:
+		background_thread: ``Thread``
+    """
+    @wraps(func)
+    def wrapper(*args, **kwags):
+        background_thread = Thread(target=func, args=(*args,))
+        background_thread.daemon = True
+        background_thread.start()
+        return background_thread
+
+    return wrapper
+
+
+
+
 
 ########################################################################################################################
 class _TimeoutError(Exception):
     """Time out error"""
     pass
-
-
-
-
-
 
 
 ########################################################################################################################
@@ -120,68 +192,5 @@ def profiler_decorator_base(fnc):
         return retval
 
     return inner
-
-
-
-########################################################################################################################
-def os_multithread(**kwargs):
-    """
-    Creating n number of threads:  1 thread per function,
-    starting them and waiting for their subsequent completion
-   Example:
-        os_multithread(function1=(function_name1, (arg1, arg2, ...)),
-                       ...)
-
-    def test_print(*args):
-        print(*args)
-
-    os_multithread(function1=(test_print, ("some text",)),
-                          function2=(test_print, ("bbbbb",)),
-                          function3=(test_print, ("ccccc",)))
-
-
-    """
-    class ThreadWithResult(Thread):
-        def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None):
-            def function():
-                self.result = target(*args, **kwargs)
-            super().__init__(group=group, target=function, name=name, daemon=daemon)
-
-    list_of_threads = []
-    for thread in kwargs.values():
-        t = ThreadWithResult(target=thread[0], args=thread[1])
-        list_of_threads.append(t)
-
-    for thread in list_of_threads:
-        thread.start()
-
-    results = []
-    for thread, keys in zip(list_of_threads, kwargs.keys()):
-        thread.join()
-        results.append((keys, thread.result))
-
-    return results
-
-
-def threading_deco(func):
-    """ A decorator to run function in background on thread
-	Args:
-		func:``function``
-			Function with args
-
-	Return:
-		background_thread: ``Thread``
-
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwags):
-        background_thread = Thread(target=func, args=(*args,))
-        background_thread.daemon = True
-        background_thread.start()
-        return background_thread
-
-    return wrapper
-
 
 
