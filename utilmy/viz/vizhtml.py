a9cd1d0b46734322041727dfc6e902f4c210f37e
arita37 <noelkev0@gmail.com>
2021-08-19 04:16:01 +0900

    utilmy/viz/vizhtml.py

--- utilmy/viz/vizhtml.py
+++ utilmy/viz/vizhtml.py
@@ -634,6 +634,13 @@ def pd_plot_scatter_highcharts(df0:pd.DataFrame, colx:str=None, coly:str=None, c
                                cfg:dict={}, mode='d3', save_img=False,  verbose=True,  **kw ):
     """ Plot Highcharts X=Y Scatter
 
+    # pip install utilmy
+    from utilmy.viz import vizhtml
+
+    vizhtml.pd_plot_scatter_highcharts(df, colx:str=None, coly:str=None, collabel=None,
+                               colclass1=None, colclass2=None, colclass3=None, nmax=10000,
+                               cfg:dict={}, mode='d3', save_img=False,  verbose=True )
+
     """
     import matplotlib
     from box import Box
