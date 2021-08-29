b71cccd4f794464c4789fdde52781fd0ad98f88c
Kev. Noel <arita37@users.noreply.github.com>
2021-08-29 18:29:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,8 +54,9 @@ git hash : https://github.com/arita37/myutil/tree/{githash}
 
 
 ### Packages  ########################################################
-#packages = ["utilmy"] + ["utilmy." + p for p in find_packages("utilmy")]
-packages = ["utilmy"] + [ p for p in  find_packages(include=['utilmy.*']) ]
+packages = ["utilmy"] + ["utilmy." + p for p in find_packages("utilmy")]
+packages = ["utilmy"] + ["utilmy.viz" + p for p in find_packages("utilmy.viz")]
+#packages = ["utilmy"] + [ p for p in  find_packages(include=['utilmy.*']) ]
 print(packages)
 
 
