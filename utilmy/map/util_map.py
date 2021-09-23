72dd1e792355989c11114e8cb74643bc18672a7f
Mohab25 <homab3@gmail.com>
2021-09-23 08:09:06 +0200

    enhancing defaults for plot_map

--- utilmy/map/util_map.py
+++ utilmy/map/util_map.py
@@ -1,7 +1,7 @@
 import folium
 from utilmy import pd_read_file
 
-def plot_map(center:list,zoom=3)->folium.folium.Map:
+def plot_map(center:list=[18,32],zoom:int=2)->folium.folium.Map:
     #folium will catch value errors for invalid numbers, no try block needed
     m = folium.Map(location=center, zoom_start=zoom)
     folium.LayerControl().add_to(m)
