18aefc68fdcca22ee5565e33b5083d6824023d35
Mohab25 <homab3@gmail.com>
2021-09-23 10:10:37 +0200

    seperate testing, files add test config for pytest

--- utilmy/map/util_map.py
+++ utilmy/map/util_map.py
@@ -1,36 +1,34 @@
-HELP = """
-
-  pip install folium
-
-
-"""
 from utilmy import pd_read_file
+try:
+    import folium
+except:
+    print('pip install folium')
 
-try :
-    import folium    
-except :
-    print("pip install folium")
-    
+HELP = """pip install folium"""
+
 def help():
     ss = ""
-    
     ss += HELP
     print(ss)
           
-############################################################################    
-def test_plot_map()->None:
-    # folium will catch value errors for invalid numbers, no try block needed
-    assert type(plot_map([35,139])) == folium.folium.Map
     
-############################################################################    
-=======
-def plot_map(center:list=[18,32],zoom:int=2)->folium.folium.Map:
+#################### Main ######################    
+
+def plot_map(center:list=[18,32],zoom:int=2)->folium.Map:
 
     #folium will catch value errors for invalid numbers, no try block needed
     m = folium.Map(location=center, zoom_start=zoom)
     folium.LayerControl().add_to(m)
     return m
     
+
+##################### Tests #####################    
+
+def test_plot_map()->None:
+    # folium will catch value errors for invalid numbers, no try block needed
+    assert type(plot_map([35,139])) == folium.Map
+
+
 # todo 
 #   adding plot_choropleth_map
 #   adding plot_geocoded_adderess 
