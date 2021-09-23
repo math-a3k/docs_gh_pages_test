1cb70457420e837c0851dd5633301f117824580b
Kev. Noel <arita37@users.noreply.github.com>
2021-09-23 21:02:12 +0900

    Update util_map.py

--- utilmy/map/util_map.py
+++ utilmy/map/util_map.py
@@ -1,10 +1,18 @@
+HELP = """
+  Keep this on top, this is also to explain the page
+  pip install folium
+
+
+
+"""
+
 from utilmy import pd_read_file
 try:
     import folium
 except:
     print('pip install folium')
 
-HELP = """pip install folium"""
+
 
 def help():
     ss = ""
@@ -13,7 +21,6 @@ def help():
           
     
 #################### Main ######################    
-
 def plot_map(center:list=[18,32],zoom:int=2)->folium.Map:
 
     #folium will catch value errors for invalid numbers, no try block needed
@@ -22,7 +29,8 @@ def plot_map(center:list=[18,32],zoom:int=2)->folium.Map:
     return m
     
 
-def plot_choropleth_map(center:list, zoom:int, geojson:str, csv:str, geojson_mutual_column:str, csv_mutual_column:str, choropleth_data_column:str, legend_title:str='Legend' ,color:str='PuBu'):
+def plot_choropleth_map(center:list, zoom:int, geojson:str, csv:str, geojson_mutual_column:str, csv_mutual_column:str, 
+                        choropleth_data_column:str, legend_title:str='Legend' ,color:str='PuBu'):
     if geojson=='' :
         print('empty path for geojson data')
         return
@@ -33,16 +41,16 @@ def plot_choropleth_map(center:list, zoom:int, geojson:str, csv:str, geojson_mut
     data_read = pd_read_file(csv)
     m = folium.Map(location=center, zoom_start=zoom)
     c = folium.Choropleth(
-    geo_data=geojson,
-    name="choropleth",
-    data=data_read,
-    columns=[f"{csv_mutual_column}", f"{choropleth_data_column}"],
-    key_on=f"feature.{geojson_mutual_column}",
-    fill_color=f"{color}",
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name=f"{legend_title}",
-)
+                geo_data=geojson,
+                name="choropleth",
+                data=data_read,
+                columns=[f"{csv_mutual_column}", f"{choropleth_data_column}"],
+                key_on=f"feature.{geojson_mutual_column}",
+                fill_color=f"{color}",
+                fill_opacity=0.7,
+                line_opacity=0.2,
+                legend_name=f"{legend_title}",
+    )
     c.add_to(m)
     return m
 
