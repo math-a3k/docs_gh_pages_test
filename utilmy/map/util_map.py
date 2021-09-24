c49909f5070ce8580817bf6e632193f3389e6f87
Mohab25 <homab3@gmail.com>
2021-09-25 01:46:23 +0200

    add two functions, spearate tests from use cases

--- utilmy/map/util_map.py
+++ utilmy/map/util_map.py
@@ -1,8 +1,16 @@
+import json
+import requests
 from utilmy import pd_read_file
+
 try:
     import folium
 except:
     print('pip install folium')
+
+try:
+    import geopandas as gdp
+except:
+    print('pip install geopandas')
 
 HELP = """pip install folium"""
 
@@ -12,7 +20,8 @@ def help():
     print(ss)
           
     
-#################### Main ######################    
+#################### Main ######################   
+# remember, you can't re-render folium map, if you have already plotted map you can't add json to it, https://github.com/python-visualization/folium/issues/906 
 
 def plot_map(center:list=[18,32],zoom:int=2)->folium.Map:
 
@@ -21,6 +30,27 @@ def plot_map(center:list=[18,32],zoom:int=2)->folium.Map:
     folium.LayerControl().add_to(m)
     return m
     
+
+def plot_geojson(geojson_path:str,center:list=[18,32],zoom:int=2):
+    m = folium.Map(location=center,zoom_start=zoom)
+    folium.GeoJson(geojson_path, name="geojson").add_to(m)
+    return m 
+
+
+def plot_topojson(topojson_path:str,json_object_name:str,center:list=[18,32],zoom:int=2):
+    m = folium.Map(location=center,zoom_start=zoom)
+    folium.TopoJson(
+    json.loads(requests.get(topojson_path).text),
+    f"objects.{json_object_name}",
+    name="topojson",
+    ).add_to(m)
+    return m
+    
+# not completed yet
+# def plot_shp(shapefile_path,center:list=[18,32],zoom:int=2):
+#     shp = gpd.read_file(shapefile_path)
+#     geojson = shp.to_file("file.json", driver="GeoJSON")
+#     plot_geojson(geojson,center,zoom)
 
 def plot_choropleth_map(center:list, zoom:int, geojson:str, csv:str, geojson_mutual_column:str, csv_mutual_column:str, choropleth_data_column:str, legend_title:str='Legend' ,color:str='PuBu'):
     if geojson=='' :
@@ -54,9 +84,48 @@ def test_plot_map()->None:
     # folium will catch value errors for invalid numbers, no try block needed
     assert type(plot_map([35,139])) == folium.Map
 
+def test_plot_geojson()->None:
+    assert type(plot_geojson) == folium.Map
+
+def test_plot_topojson()->None:
+    assert type(plot_topojson) == folium.Map
+
+def test_plot_choropleth_map()->None:
+    assert type(plot_choropleth_map) == folium.Map
+
+
+##################### Use cases #################    
+    # should be used in a web run env as jupyter notebook to see the actual map. 
+
+# plot_map
+def plot_map_use_case():
+    plot_map([15,34],12)
+
+
+# plot_geojson
+def plot_geojson_use_case():
+    url = 'http://enjalot.github.io/wwsd/data/world/world-110m.geojson'
+    plot_geojson(url)
+
+
+# plot_topojson
+def plot_topojson_use_case():
+    url = 'https://unpkg.com/world-atlas@1.1.4/world/110m.json'
+    plot_topojson(url,'countries')
+
+# plot_choropleth_map
+def plot_choropleth_map_use_case():
+    csv=  'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/US_Unemployment_Oct2012.csv'
+    geojson = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json'
+    plot_choropleth_map([48, -102],3,geojson,csv,'id','State','Unemployment')
+
+
 
 # todo 
 #   adding tests and docs to plot_choropleth_map
+#   adding GeoJSON.
+#   adding Vector Data (shp)
+#   adding raster data. 
 #   adding plot_geocoded_adderess 
 
 
