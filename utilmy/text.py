ff5c541f36e19d322d3f72e35f9763b0fa9fcfff
arita37 <noelkev0@gmail.com>
2021-10-01 21:07:41 +0900

    utilmy/text.py

--- utilmy/text.py
+++ utilmy/text.py
@@ -100,9 +100,7 @@ def pd_text_hash_create_lsh(df, col, sep=" ", threshold=0.7, num_perm=10, npool=
         for xi in res :
             hash_lines.extend(xi[0])
 
-        res = sum(res, [])  ### flatten the list
         return hash_lines, lsh
-        
         
     if len(df) < 1 :
         return [],[]
