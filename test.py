5db33f91f084fa01cd610538822db5c9d9579ad0
Kev. Noel <arita37@users.noreply.github.com>
2021-10-04 22:26:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,79 +1,62 @@
-# pylint: disable=C0321,C0103,C0301,E1305,E1121,C0302,C0330,C0111,W0613,W0611,R1705
+"""
+    #### python test.py   test_all
+    #### python test.py   test_viz_vizhtml      
+      
+  
+    test_utilmy_pd_os_session()
+    test_decorators_os()
+    # test_tabular_test()
+    test_text_similarity()
+    test_docs_cli()
+      
+    test_viz_vizhtml()   
+
+"""
 # -*- coding: utf-8 -*-
-import os, sys, time, datetime,inspect
+import os, sys, time, datetime,inspect, random
+import pandas as pd, random, numpy as np 
 
 def log(*s):
    print(*s, flush=True)
 
-#########################################################################################
 def pd_random(ncols=7, nrows=100):
-   import pandas as pd, random
    ll = [[ random.random() for i in range(0, ncols)] for j in range(0, nrows) ]
    df = pd.DataFrame(ll, columns = [str(i) for i in range(0,ncols)])
    return df
 
+
+def pd_generate_data(ncols=7, nrows=100):
+    """
+    Generate sample data for function testing
+    categorical features for anova test
+    """
+    np.random.seed(444) 
+    numerical    = [[ random.random() for i in range(0, ncols)] for j in range(0, nrows) ]
+    df = pd.DataFrame(numerical, columns = [str(i) for i in range(0,ncols)])
+    df['cat1']= np.random.choice(  a=[0, 1],  size=100,  p=[0.7, 0.3]  ) 
+    df['cat2']= np.random.choice(  a=[4, 5, 6],  size=100,  p=[0.5, 0.3, 0.2]  ) 
+    df['cat1']= np.where( df['cat1'] == 4,'low',np.where(df['cat1'] == 5, 'High','V.High'))
+    return df
 
 
 
 #########################################################################################
 #########################################################################################
 def test_utilmy_plot():
-    df = pd_random(7, 100)
     from utilmy import pd_plot_multi
+    df = pd_random(7, 100)  
     pd_plot_multi(df, cols_axe1=['0', '1'])
 
 
 def test_utilmy_pd_os_session():
    from utilmy import (pd_show, git_current_hash, )
 
-   ############################################################################
-   from utilmy import pd_read_file
-   import pandas as pd, random
-   ncols = 7
-   nrows = 100
-   ll = [[ random.random() for i in range(0, ncols)] for j in range(0, nrows) ]
-   # Required for it to be detected in Session's globals()
-   global df
-   df = pd.DataFrame(ll, columns = [str(i) for i in range(0,ncols)])
-   n0 = len(df)
-   s0 = df.values.sum()
-   os.makedirs("data/parquet/", exist_ok= True)
-
-   ##### m_job , n_pool tests  ##############################
-   ncopy = 20
-   for i in range(0, ncopy) :
-      df.to_csv( f"data/parquet/ppf_{i}.csv.gz",  compression='gzip' , index=False)
-
-   df1 = pd_read_file("data/parquet/ppf*.gz", verbose=1, n_pool= 7 )
-
-   assert len(df1) == ncopy * n0,         f"df1 {len(df1) }, original {n0}"
-   assert round(df1.values.sum(), 5) == round(ncopy * s0,5), f"df1 {df1.values.sum()}, original {ncopy*s0}"
-
-
-   ####################################################
-   df.to_csv( "data/parquet/fa0b2.csv.gz",   compression='gzip' , index=False)
-   df.to_csv( "data/parquet/fab03.csv.gz",   compression='gzip' , index=False)
-   df.to_csv( "data/parquet/fabc04.csv.gz",  compression='gzip' , index=False)
-   df.to_csv( "data/parquet/fa0bc05.csv.gz", compression='gzip' , index=False)
-
-   df1 = pd_read_file("data/parquet/fab*.*", verbose=1)
-   assert len(df1) == 2 * n0, f"df1 {len(df1) }, original {n0}"
-
-
-   ##### Stresss n_pool
-   df2 = pd_read_file("data/parquet/fab*.*", n_pool=1000 )
-   assert len(df2) == 2 * n0, f"df1 {len(df2) }, original {n0}"
-
-   
-   ###################################################################################
    ###################################################################################
    from utilmy import git_repo_root
    print(git_repo_root())
    assert not git_repo_root() == None, "err git repo"
 
 
-
-   ###################################################################################
    ###################################################################################
    from utilmy import os_makedirs, os_system, os_removedirs
    os_makedirs('ztmp/ztmp2/myfile.txt')
@@ -101,7 +84,6 @@ def test_utilmy_pd_os_session():
    
 
    ###################################################################################
-   ###################################################################################
    from utilmy import global_verbosity
    print('verbosity', global_verbosity(__file__, "config.json", 40,))
    print('verbosity', global_verbosity('../', "config.json", 40,))
@@ -114,8 +96,7 @@ def test_utilmy_pd_os_session():
    assert gverbosity == verbosity, "incorrect verbosity "
 
    
-   
-   
+      
 def test_utilmy_session():   
    from utilmy import Session
    sess = Session("ztmp/session")
@@ -139,10 +120,8 @@ def test_utilmy_session():
 
    
 
-
 def test_docs_cli():
-    """
-      from utilmy.docs.generate_doc import run_markdown, run_table
+    """  from utilmy.docs.generate_doc import run_markdown, run_table
     """
     cmd = "doc-gen  --repo_dir utilmy/      --doc_dir docs/"
     os.system(cmd)
@@ -152,18 +131,7 @@ def test_docs_cli():
 
 #########################################################################################
 #########################################################################################
-def test_decorators_os(*args):
-    from utilmy.parallel import multithread_run_list
-    def test_print(*args):
-        print(args[0]*args[0])
-        return args[0]*args[0]
-
-    assert multithread_run_list(function1=(test_print, (5,)),
-                          function2=(test_print, (4,)),
-                          function3=(test_print, (2,)))
-
-      
-    ###################################################################################
+def test_decorators_os(*args):      
     from utilmy.decorators import profiler_decorator, profiler_context
 
     @profiler_decorator
@@ -199,32 +167,10 @@ def test_decorators_os(*args):
     a.method()
       
       
-      
-      
-      
-      
-      
-#########################################################################################
+   
 #######################################################################################
-def pd_generate_data(ncols=7, nrows=100):
-    """
-    Generate sample data for function testing
-    categorical features for anova test
-    """
-    import pandas as pd, random
-    import numpy as np 
-    np.random.seed(444) 
-    numerical = [[ random.random() for i in range(0, ncols)] for j in range(0, nrows) ]
-    categorical2 =  data = np.random.choice(  a=[0, 1],  size=100,  p=[0.7, 0.3]  ) 
-    categorical1 =  data = np.random.choice(  a=[4, 5, 6],  size=100,  p=[0.5, 0.3, 0.2]  ) 
-    df = pd.DataFrame(numerical, columns = [str(i) for i in range(0,ncols)])
-    df['cat1']=categorical1
-    df['cat2']=categorical2
-    df['cat1']= np.where( df['cat1'] == 4,'low',np.where(df['cat1'] == 5, 'High','V.High'))
-    return df
-
-
-def test_tabular_test():
+#######################################################################################
+def test_tabular():
         """
         ANOVA test
         """
@@ -235,8 +181,7 @@ def test_tabular_test():
         test_plot_qqplot(df, '1')
 
 
-
-         
+        
 #########################################################################################
 #########################################################################################
 def test_text_similarity():
@@ -244,7 +189,6 @@ def test_text_similarity():
     from difflib import SequenceMatcher
     from pandas._testing import assert_series_equal
 
-    import pandas as pd
     list1 = ['dog', 'cat']
     list2 = ['doggy', 'cat']
 
@@ -307,16 +251,5 @@ def test_all():
 if __name__ == "__main__":
     import fire
     fire.Fire()    
-    #### python test.py   test_all
-    #### python test.py   test_viz_vizhtml      
-      
-"""   
-    test_utilmy_pd_os_session()
-    test_decorators_os()
-    # test_tabular_test()
-    test_text_similarity()
-    test_docs_cli()
-      
-    test_viz_vizhtml()   
-"""
+
 
