b7c62a757e1af4106b93c8baf4fcd7ab53c558d5
arita37 <noelkev0@gmail.com>
2021-10-05 14:44:58 +0900

    ppandas.py

--- utilmy/ppandas.py
+++ utilmy/ppandas.py
@@ -1,6 +1,6 @@
 # pylint: disable=C0321,C0103,C0301,E1305,E1121,C0302,C0330,C0111,W0613,W0611,R1705
 # -*- coding: utf-8 -*-
-import os, sys, time, datetime,inspect, json, yaml, gc
+import os, sys, time, datetime,inspect, json, yaml, gc, pandas as pd, numpy as np
 
 
 def log(*s):
