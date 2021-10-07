1b58d9d0745d3b176e0ce7944e830e34eac9a6bb
Kev. Noel <arita37@users.noreply.github.com>
2021-10-08 01:42:49 +0900

    Update util_dl.py

--- utilmy/deeplearning/util_dl.py
+++ utilmy/deeplearning/util_dl.py
@@ -113,7 +113,96 @@ def gpu_free():
     return deviceid_free
             
             
-            
+   
+
+def down_page(query, out_dir="query1", genre_en='', id0="", cat="", npage=1) :
+    """
+        python prepro.py down_page  'メンス+ポロシャツ'    --out_dir men_fs_blue  
+
+
+    """
+    import time, os, json, csv, requests, sys, urllib
+    from bs4 import BeautifulSoup as bs
+    from urllib.request import Request, urlopen
+    import urllib.parse
+
+
+    path = "/datrakuten/" + out_dir + "/"
+    os.makedirs(path, exist_ok=True)
+    # os.chdir(path)
+
+    query2     = urllib.parse.quote(query, encoding='utf-8')
+    url_prefix = 'httpl/' + query2
+    ### https://search.rakuten.co.jp/search/mall/%E3%83%A1%E3%8384+blue+/?p=2
+    print(url_prefix)
+    print(path)
+
+    csv_file   = open( path + 'ameta.csv','w',encoding="utf-8")
+    csv_writer = csv.writer(csv_file, delimiter='\t')
+    csv_writer.writerow(['path', 'id0', 'cat', 'genre_en', 'image_name', 'price','shop','item_url','page_url',  ])
+
+    page  = 1
+    count = 0
+    while page < npage+1 :
+        try:
+            rakuten_url = url_prefix  + f"/?p=+{page}"
+            req    = Request(url=rakuten_url)
+            source = urlopen(req).read()
+            soup   = bs(source,'lxml')
+
+            print('page', page, str(soup)[:5], str(rakuten_url)[-20:],  )
+
+            for individual_item in soup.find_all('div',class_='searchresultitem'):
+                count += 1
+                save = 0
+                shopname     = 'nan'
+                count_review = 'nan'
+
+                for names in individual_item.find_all('div',class_='title'):
+                    product_name = names.h2.a.text
+                    break
+
+                for price in individual_item.find_all('div',class_='price'):
+                    product_price = price.span.text
+                    product_price = product_price .replace("円", "").replace(",", "") 
+                    break
+                
+                for url in individual_item.find_all('div',class_='image'):
+                    product_url = url.a.get('href')
+                    break
+
+                for images in individual_item.find_all('div',class_='image'):
+                    try:
+                        product_image = images.a.img.get('src')
+                        urllib.request.urlretrieve(product_image, path + str(count)+".jpg")
+                        # upload_to_drive(str(count)+'.jpg')
+                        count += 1
+                        break
+                    except:
+                        save = 1
+                        print(product_url + " Error Detected")
+                    
+                for simpleshop in individual_item.find_all('div',class_='merchant'):
+                    shopname = simpleshop.a.text
+                    break
+
+                for review in individual_item.find_all('a',class_='dui-rating-filter'):
+                    count_review = review.text
+
+                if save == 0:
+                    csv_writer.writerow([str(count)+'.jpg', id0, cat, genre_en,  product_name, product_price, shopname, product_url, rakuten_url, ])
+
+        except Exception as e :
+            print(e)
+            time.sleep(2)
+            continue
+
+        page += 1
+
+    print("Success", page-1, count)
+
+
+
 
 
 
