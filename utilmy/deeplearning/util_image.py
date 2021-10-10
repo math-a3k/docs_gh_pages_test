f834e511aaf6ae02f3313fb7a02f4ac1d1c3c5f6
arita37 <noelkev0@gmail.com>
2021-10-10 21:31:06 +0900

    util_image.py

--- utilmy/deeplearning/util_image.py
+++ utilmy/deeplearning/util_image.py
@@ -6,18 +6,19 @@ HELP="""
 """
 import os,io, numpy as np, sys, glob, time, copy, json, functools, pandas as pd
 from typing import Union
-
+from box import Box
 
 os.environ['MPLCONFIGDIR'] = "/tmp/"
 
 import io, cv2,  tifffile.tifffile
-import tensorflow as tf, tensorflow_addons as tfa
-from tensorflow.python.keras.utils.data_utils import Sequence    
-from PIL import Image 
-from albumentations.core.transforms_interface import ImageOnlyTransform
+from PIL import Image
 from skimage import morphology
-from box import Box
 import diskcache as dc
+
+try :
+   from albumentations.core.transforms_interface import ImageOnlyTransform
+
+except : pass
 
 
 
@@ -39,7 +40,7 @@ def help():
     from utilmy import help_create
     ss  = ""
     ss += HELP
-    ss += help_create("utilmy.deeplearning.util_dl")
+    ss += help_create("utilmy.deeplearning.util_image")
     print(ss)
 
 
@@ -53,7 +54,6 @@ def test():
 
 
 
-################################################################################################
 ################################################################################################
 def prepro_image(image_path:str, xdim=1, ydim=1):
     mean   = [0.5]
@@ -74,13 +74,78 @@ def prepro_image(image_path:str, xdim=1, ydim=1):
 
 
 
-def prepro_images(image_paths, nmax=10000000):
-  images = []
-  for i in range(len(image_paths)):
-    if i > nmax : break
-    image =  prepro_image(image_paths[i])
-    images.append(image)
-  return images
+def prepro_images(image_path_list:list, prepro_image_fun=None, npool=1):
+    """ Parallel processing
+
+    """
+    from multiprocessing.dummy import Pool    #### use threads for I/O bound tasks
+
+    pool = Pool(npool)
+    res  = pool.map(prepro_image_fun, image_path_list)
+    pool.close() ;     pool.join()  ; pool = None
+
+    print('len res', len(res))
+    images, labels = [], []
+    for (x,y) in res :
+        if len(y)> 0 and len(x)> 0 :
+            images.append(x)
+            labels.append(y)
+
+    print('len images', len(images))
+    print(str(labels)[:60])
+    return images, labels
+
+
+
+
+################################################################################################
+def image_read(filepath_or_buffer: Union[str, io.BytesIO]):
+    """
+    Read a file into an image object
+    Args:
+        filepath_or_buffer: The path to the file, a URL, or any object
+            with a `read` method (such as `io.BytesIO`)
+    """
+    image = None
+    if isinstance(filepath_or_buffer, np.ndarray):
+        return filepath_or_buffer
+
+    if hasattr(filepath_or_buffer, "read"):
+        image = np.asarray(bytearray(filepath_or_buffer.read()), dtype=np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
+
+    elif isinstance(filepath_or_buffer, str):
+        assert os.path.isfile(filepath_or_buffer), (
+            "Could not find image at path: " + filepath_or_buffer
+        )
+        if filepath_or_buffer.endswith(".tif") or filepath_or_buffer.endswith(".tiff"):
+            image = tifffile.imread(filepath_or_buffer)
+        else:
+            image = cv2.imread(filepath_or_buffer)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    return image
+
+
+
+def image_save():
+    ##### Write some sample images  ########################
+    import diskcache as dc
+    db_path = "/data/workspaces/noelkevin01/img/data/fashion/train_npz/small/img_train_r2p2_70k_clean_nobg_256_256-100000.cache"
+    cache   = dc.Cache(db_path)
+    print('Nimages', len(cache) )
+
+    log('### writing on disk  ######################################')
+    dir_check = out_dir + f"/{xname}/"
+    os.makedirs(dir_check, exist_ok=True)
+    for i, key in enumerate(img_list) :
+        if i > 10: break
+        img = cache[key]
+        img = img[:, :, ::-1]
+        key2 = key.split("/")[-1]
+        cv2.imwrite( dir_check + f"/{i}_{key2}"  , img)
+    log( dir_check )
+
 
 
 def image_center_crop(img, dim):
@@ -98,34 +163,6 @@ def image_center_crop(img, dim):
     cw2, ch2 = int(crop_width/2), int(crop_height/2)
     crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
     return crop_img
-
-
-
-
-def prepro_images_multi(image_paths, npool=30, prepro_image=None):
-    """ Parallel processing
-
-    """
-    from multiprocessing.dummy import Pool    #### use threads for I/O bound tasks
-
-    pool = Pool(npool)
-    res  = pool.map(prepro_image, image_paths)
-    pool.close()
-    pool.join()
-
-    print('len res', len(res))
-    images, labels = [], []
-    for (x,y) in res :
-        if len(y)> 0 and len(x)> 0 :
-            images.append(x)
-            labels.append(y)
-
-    print('len images', len(images))
-    print(str(labels)[:60])
-    return images, labels
-
-
-
 
 
 
@@ -175,105 +212,14 @@ def image_resize_pad(img,size=(256,256), padColor=0 ):
 
 
 
-
-
-
-def image_face_blank(in_dir="", level = "/*",
-
-                     out_dir=f"", npool=30):
-    """  Remove face
-     python prepro.py  image_face_blank
-
-     python prepro.py  image_face_blank  --in_dir img/data/fashion/test_nobg   --out_dir img/data/fashion/test_nobg_noface
-     python prepro.py  image_face_blank  --in_dir img/data/fashion/train_nobg   --out_dir img/data/fashion/train_nobg_noface
-      five elements are [xmin, ymin, xmax, ymax, detection_confidence]
-    """
-    import cv2, glob
-    import face_detection
-
-    #in_dir  = "/data/workspaces/noelkevin01/" + in_dir
-    #out_dir = "/data/workspaces/noelkevin01/" + out_dir
-    npool    = 30
-    in_dir   = "/data//"
-    out_dir  = "/data/wonoface/"
-    fpaths   = glob.glob(in_dir + "/*/*" )
-
-    # fpaths   = [  t for t in fpath if "/-1" not in fpaths ]
-    # fpaths   = fpaths[:60]
-
-    detector = face_detection.build_detector( "RetinaNetMobileNetV1",
-                            confidence_threshold=.5, nms_iou_threshold=.3)
-
-    log(str(fpaths)[:60])
-
-    def myfun(fp):
-      try :
-          log(fp)
-          img   = cv2.imread(fp)
-          im    = img[:, :, ::-1]
-          areas = detector.detect(im)
-
-          ### list of areas where face is detected.
-          for (x0, y0, x1, y1, proba) in areas:
-             x0,y0, x1, y1     = int(x0), int(y0), int(x1), int(y1)
-             img[y0:y1, x0:x1] = 0
-
-          fout = fp.replace(in_dir, out_dir)
-          os.makedirs( os.path.dirname(fout), exist_ok=True)
-          cv2.imwrite( fout, img )
-      except : pass
-
-
-    #for fp in fpaths :
-    #  myfun(fp)
-
-    from multiprocessing.dummy import Pool    #### use threads for I/O bound tasks
-    pool = Pool(npool)
-    res  = pool.map(myfun, fpaths)
-    pool.close()
-    pool.join()
-
-
-
-
-def image_read(filepath_or_buffer: Union[str, io.BytesIO]):
-    """
-    Read a file into an image object
-    Args:
-        filepath_or_buffer: The path to the file, a URL, or any object
-            with a `read` method (such as `io.BytesIO`)
-    """
-    image = None
-    if isinstance(filepath_or_buffer, np.ndarray):
-        return filepath_or_buffer
-
-    if hasattr(filepath_or_buffer, "read"):
-        image = np.asarray(bytearray(filepath_or_buffer.read()), dtype=np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
-
-    elif isinstance(filepath_or_buffer, str):
-        assert os.path.isfile(filepath_or_buffer), (
-            "Could not find image at path: " + filepath_or_buffer
-        )
-        if filepath_or_buffer.endswith(".tif") or filepath_or_buffer.endswith(".tiff"):
-            image = tifffile.imread(filepath_or_buffer)
-        else:
-            image = cv2.imread(filepath_or_buffer)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    return image
-
-
-
-def image_save_tocache(out_dir, name="cache1"):
-    ##### Write some sample images  ########################
+def image_cache_check(db_path:str="db_images.cache", dirout:str="tmp/", tag="cache1"):
+    ##### Write some sample images  from cache #############################
     import diskcache as dc
-    db_path = "/data/woshion/train_npz/small/img_train_r2p2_70k_clean_nobg_256_256-100000.cache"
-    cache   = dc.Cache(db_path)
-    print('Nimages', len(cache) )
+    cache   = dc.Cache(db_path, size_limit= 100 * 10**9, timeout= 5 )
+    log('Nimages', len(cache) )
 
-    log('### writing on disk  ######################################')
-    dir_check = out_dir + f"/{name}/"
+    log('### Check writing on disk  ###########################')
+    dir_check = dirout + f"/{tag}/"
     os.makedirs(dir_check, exist_ok=True)
     for i, key in enumerate(cache) :
         if i > 10: break
@@ -286,15 +232,23 @@ def image_save_tocache(out_dir, name="cache1"):
 
 
 
+def image_cache_save(image_path_list:str="db_images.cache", db_dir:str="tmp/", tag="cache1"):
+    ##### Write some sample images  from cache #############################
+    import diskcache as dc
+    cache   = dc.Cache(db_dir, size_limit= 100 * 10**9, timeout= 5 )
+    log('Nimages', len(cache) )
 
 
+    log('### Check writing on disk  ###########################')
+    for img_path in image_path_list:
+        img = image_read(img_path)
+        cache[img_path] = img
 
 
 
 def image_check_npz(path_npz,  keys=['train'], path="", tag="", n_sample=3,
                     renorm=True):    
     import cv2
-    
     os.makedirs(path, exist_ok=True)
     data_npz = np.load( path_npz  )
     keys     =  data_npz.keys() if keys is None else keys 
@@ -312,7 +266,7 @@ def image_check_npz(path_npz,  keys=['train'], path="", tag="", n_sample=3,
     
     
     
-def padding_generate(
+def image_padding_generate(
     paddings_number: int = 1, min_padding: int = 1, max_padding: int = 1
 ) -> np.array:
     """
@@ -324,70 +278,6 @@ def padding_generate(
     """
     return np.random.randint(low=min_padding, high=max_padding + 1, size=paddings_number)
 
-
-
-
-def image_center_crop(img, dim):
-	"""Returns center cropped image
-	Args:
-	img: image to be center cropped
-	dim: dimensions (width, height) to be cropped
-	"""
-	width, height = img.shape[1], img.shape[0]
-
-	# process crop width and height for max available dimension
-	crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
-	crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0] 
-	mid_x, mid_y = int(width/2), int(height/2)
-	cw2, ch2 = int(crop_width/2), int(crop_height/2) 
-	crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
-	return crop_img
-
-
-
-def image_resize_pad(img,size=(256,256), padColor=0 ):
-    """
-      resize and keep into the target Box
-    
-    """
-
-    h, w = img.shape[:2]
-    sh, sw = size
-
-    # interpolation method
-    if h > sh or w > sw: # shrinking image
-        interp = cv2.INTER_AREA
-
-    else: # stretching image
-        interp = cv2.INTER_CUBIC
-
-    # aspect ratio of image
-    aspect  = float(w)/h 
-    saspect = float(sw)/sh
-
-    if (saspect > aspect) or ((saspect == 1) and (aspect <= 1)):  # new horizontal image
-        new_h = sh
-        new_w = np.round(new_h * aspect).astype(int)
-        pad_horz = float(sw - new_w) / 2
-        pad_left, pad_right = np.floor(pad_horz).astype(int), np.ceil(pad_horz).astype(int)
-        pad_top, pad_bot = 0, 0
-
-    elif (saspect < aspect) or ((saspect == 1) and (aspect >= 1)):  # new vertical image
-        new_w = sw
-        new_h = np.round(float(new_w) / aspect).astype(int)
-        pad_vert = float(sh - new_h) / 2
-        pad_top, pad_bot = np.floor(pad_vert).astype(int), np.ceil(pad_vert).astype(int)
-        pad_left, pad_right = 0, 0
-
-    # set pad color
-    if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)): # color image but only one color provided
-        padColor = [padColor]*3
-
-    # scale and pad
-    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
-    scaled_img = cv2.copyMakeBorder(scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
-
-    return scaled_img
 
 
 
@@ -486,45 +376,6 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # Return the resized image
     return cv2.resize(image, dim, interpolation=inter)
 
-
-def image_read(filepath_or_buffer: Union[str, io.BytesIO]):
-    """
-    Read a file into an image object
-    Args:
-        filepath_or_buffer: The path to the file, a URL, or any object
-            with a `read` method (such as `io.BytesIO`)
-    """
-    image = None
-    if isinstance(filepath_or_buffer, np.ndarray):
-        return filepath_or_buffer
-
-    if hasattr(filepath_or_buffer, "read"):
-        image = np.asarray(bytearray(filepath_or_buffer.read()), dtype=np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
-
-    elif isinstance(filepath_or_buffer, str):
-        assert os.path.isfile(filepath_or_buffer), (
-            "Could not find image at path: " + filepath_or_buffer
-        )
-        if filepath_or_buffer.endswith(".tif") or filepath_or_buffer.endswith(".tiff"):
-            image = tifffile.imread(filepath_or_buffer)
-        else:
-            image = cv2.imread(filepath_or_buffer)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            
-    return image  
-
-
-
-
-
-
-
-
-
-
- 
-    
 
 def image_remove_bg(in_dir="", out_dir="", level=1):
     """ #### remove background
@@ -751,23 +602,6 @@ def image_text_blank(in_dir, out_dir, level="/*"):
       except : pass
     
 
-def image_save():
-    ##### Write some sample images  ########################
-    import diskcache as dc
-    db_path = "/data/workspaces/noelkevin01/img/data/fashion/train_npz/small/img_train_r2p2_70k_clean_nobg_256_256-100000.cache"
-    cache   = dc.Cache(db_path)
-    print('Nimages', len(cache) )
-
-    log('### writing on disk  ######################################')
-    dir_check = out_dir + f"/{xname}/"
-    os.makedirs(dir_check, exist_ok=True)
-    for i, key in enumerate(img_list) :
-        if i > 10: break       
-        img = cache[key]
-        img = img[:, :, ::-1]
-        key2 = key.split("/")[-1]
-        cv2.imwrite( dir_check + f"/{i}_{key2}"  , img)            
-    log( dir_check ) 
 
 
     
--- utilmy/oos.py
+++ utilmy/oos.py
@@ -256,37 +256,35 @@ def os_walk(path, pattern="*", dirlevel=50):
     return  matches
 
 
-def os_copy_safe(dirin=None, dirout=None,  nlevel=5, nfile=5000, logdir="./", pattern="*", cmd_fallback=""):  \
-    """    
-        Copy Safe between drives
+def os_copy_safe(dirin:str=None, dirout:str=None,  nlevel=5, nfile=50000, logdir="./", pattern="*", cmd_fallback=""):
+    """
+        Copy Safe between drives, remount drive when needed
     """
     import shutil, time, os, glob
-    # dirin  = "/data/workspaces/takos01/cpa/emb/"
-    # dirout = "/a/adigcb204/ipsvolh03/ndata/cpa/emb/emb/"
     if dirin is None :
        dirin  = "/tmp/"
-       dirout = "/tmp"    
-    
-    flist = [] ; dirinj = dirin        
-    for j in range(nlevel): 
+       dirout = "/tmp"
+
+    flist = [] ; dirinj = dirin
+    for j in range(nlevel):
         dirinj = dirinj + "/" + pattern
         ztmp =  sorted( glob.glob(dirinj ) )
         if len(ztmp) < 1 : break
         flist  = flist + ztmp
-        
-        
+
+
     log('n files', len(flist), dirinj, dirin )
-    kk = 1 ; ntry = 0 ;i =0 
+    kk = 1 ; ntry = 0 ;i =0
     for i in range(0, len(flist)) :
         fi  = flist[i]
         fi2 = fi.replace(dirin, dirout)
-        
+
         if not fi.isascii(): continue
-        
+
         if not os.path.isfile(fi2) and os.path.isfile(fi) :
              kk = kk + 1
-             if kk > nfile   : return 1   
-             if kk % 50 == 0 : time.sleep(0.5)             
+             if kk > nfile   : return 1
+             if kk % 50 == 0 : time.sleep(0.5)
              if kk % 10 : log(fi2)
              os.makedirs(os.path.dirname(fi2), exist_ok=True)
              try :
@@ -301,7 +299,7 @@ def os_copy_safe(dirin=None, dirout=None,  nlevel=5, nfile=5000, logdir="./", pa
                 time.sleep(10)
                 i = i - 1
                 ntry = ntry + 1
-    log('Finished', i)   
+    log('Finished', i)
 
                 
 def z_os_search_fast(fname, texts=None, mode="regex/str"):
