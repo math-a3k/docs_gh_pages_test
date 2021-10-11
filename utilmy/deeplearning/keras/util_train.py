9b27624126f76f33740439f12bca5027c818f6e1
Maryam-confiz <maryam.sana@confiz.com>
2021-10-11 21:22:58 +0500

    all root imports done

--- utilmy/deeplearning/keras/util_train.py
+++ utilmy/deeplearning/keras/util_train.py
@@ -119,7 +119,7 @@ def os_path_copy(in_dir, path, ext="*.py"):
 
 os_path_copy(in_dir= cc.code_source  , path= cc.model_dir2 + "/code/")
 
-from train_graph_loss import metric_accuracy;
+from utilmy.deeplearning.keras.train_graph_loss import metric_accuracy;
 
 
 def check_valid_image(img_list, path="", tag="", y_labels="", n_sample=3, renorm=True):
