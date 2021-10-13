361f3fe5c7d5c605dba813cae40eaa1c75b72dfb
arita37 <arita37@users.noreply.github.com>
2021-10-13 05:19:45 +0000

    Auto Docs
    Signed-off-by: GitHub Actions <actions@github.com>

--- docs/flake8_syntax_error.py
+++ docs/flake8_syntax_error.py
@@ -4,3 +4,8 @@
             ^
 1     F402 import 'dc' from line 13 shadowed by loop variable
 1
+./utilmy/deeplearning/util_topk.py:420:13: F402 import 'dc' from line 13 shadowed by loop variable
+            import diskcache as dc
+            ^
+1     F402 import 'dc' from line 13 shadowed by loop variable
+1
