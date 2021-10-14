0059744a3db3ae0a45a97fa435285befd891ffd6
Kev. Noel <arita37@users.noreply.github.com>
2021-10-15 01:56:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -13,8 +13,9 @@ import os, sys, time, datetime,inspect, random, pandas as pd, random, numpy as n
 
 
 
+
 #### NEVER IMPORT HERE  !!!!
-### from utilmy import pd_random, pd_generate_data
+# from utilmy import pd_random, pd_generate_data
 # from utilmy.tabular import pd_data_drift_detect
 # from tensorflow.python.ops.gen_array_ops import one_hot
 
@@ -28,7 +29,25 @@ def import_module(mname:str='utilmy.oos'):
     return m
 
    
-   
+def pd_random(ncols=7, nrows=100):
+   import pandas as pd
+   ll = [[ random.random() for i in range(0, ncols)] for j in range(0, nrows) ]
+   df = pd.DataFrame(ll, columns = [str(i) for i in range(0,ncols)])
+   return df
+
+
+def pd_generate_data(ncols=7, nrows=100):
+    """ Generate sample data for function testing
+    categorical features for anova test
+    """
+    import numpy as np, pandas as pd
+    np.random.seed(444)
+    numerical    = [[ random.random() for i in range(0, ncols)] for j in range(0, nrows) ]
+    df = pd.DataFrame(numerical, columns = [str(i) for i in range(0,ncols)])
+    df['cat1']= np.random.choice(  a=[0, 1],  size=nrows,  p=[0.7, 0.3]  )
+    df['cat2']= np.random.choice(  a=[4, 5, 6],  size=nrows,  p=[0.5, 0.3, 0.2]  )
+    df['cat1']= np.where( df['cat1'] == 0,'low',np.where(df['cat1'] == 1, 'High','V.High'))
+    return df   
    
    
    
