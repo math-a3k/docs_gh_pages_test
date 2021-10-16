beff49faafb66ce72f6b977cd9e5be40bbf305aa
arita37 <arita37@users.noreply.github.com>
2021-10-16 01:37:00 +0000

    Auto Docs
    Signed-off-by: GitHub Actions <actions@github.com>

--- docs/flake8_syntax_error.py
+++ docs/flake8_syntax_error.py
@@ -52,3 +52,8 @@ RESIDUAL BLOCK  ################################################################
 1
 0
 0
+./utilmy/deeplearning/keras/util_layers.py:94:60: E999 SyntaxError: invalid syntax
+    for class_name, num_classes in label_name_ncount.items()
+                                                           ^
+1     E999 SyntaxError: invalid syntax
+1
