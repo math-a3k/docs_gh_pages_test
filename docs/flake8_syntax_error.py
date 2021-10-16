d0eb564334a8fe128b5624f61909c9942ecf20ae
Kev. Noel <arita37@users.noreply.github.com>
2021-10-16 13:05:07 +0900

    Update flake8_syntax_error.py

--- docs/flake8_syntax_error.py
+++ docs/flake8_syntax_error.py
@@ -1,9 +1,1 @@
 
-./utilmy/deeplearning/keras/util_layers.py:94:60: E999 SyntaxError: invalid syntax
-    for class_name, num_classes in label_name_ncount.items()
-                                                           ^
-1     E999 SyntaxError: invalid syntax
-1
-0
-0
-0
