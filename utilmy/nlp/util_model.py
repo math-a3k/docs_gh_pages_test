7608267c1518b824878bd69be47b5d694d70f8fb
Kev. Noel <arita37@users.noreply.github.com>
2021-10-18 08:53:17 +0900

    Update util_model.py

--- utilmy/nlp/util_model.py
+++ utilmy/nlp/util_model.py
@@ -24,19 +24,24 @@ def test_gensim1():
     log("test_gensim")
     dir0 = os.getcwd()  
     pars = Box({})
-    pars.min_n=6 ;  pars.max_n=6; pars.window=3
+    pars.min_n=6 ;  pars.max_n=6; pars.window=3;  pars.vector_size=3
    
     text_generate_random_sentences(dirout=  dir0 + '/testdata/mytext1.txt')
-    gensim_model_train_save(dirout= dir0 + '/modelout1/model.bin', dirinput=  dir0 +  '/testdata/mytext1.txt', epochs=1)
+    gensim_model_train_save(None, dirout= dir0 + '/modelout1/model.bin', dirinput=  dir0 +  '/testdata/mytext1.txt', epochs=1,
+                            pars = pars)
     #gensim_model_check(dir0 + '/modelout1/model.bin')
    
    
-    model = gensim_model_load( dir0 + '/modelout1/model.bin')
-    text_generate_random_sentences(dirout=  dir0 +  '/testdata/mytext2.txt')      
-    gensim_model_train_save(model, dirout= dir0 + '/modelout2/model.bin', dirinput= dir0 +  '/testdata/mytext2.txt', epochs=1)
+    # model = gensim_model_load( dir0 + '/modelout1/model.bin')
+    text_generate_random_sentences( dirout=  dir0 +  '/testdata/mytext2.txt')      
+    gensim_model_train_save(model_or_path = dir0 + '/modelout1/model.bin', dirout= dir0 + '/modelout2/model.bin', dirinput= dir0 +  '/testdata/mytext2.txt', epochs=1)
     # gensim_model_check(dir0 +  '/modelout2/model.bin')
 
 
+    model = gensim_model_load( dir0 + '/modelout2/model.bin')
+    text_generate_random_sentences( dirout=  dir0 +  '/testdata/mytext2.txt')      
+    gensim_model_train_save(model_or_path = model, dirout= dir0 + '/modelout2/model.bin', dirinput= dir0 +  '/testdata/mytext2.txt', epochs=1)
+    # gensim_model_check(dir0 +  '/modelout2/model.bin')
 
       
       
@@ -92,8 +97,10 @@ def gensim_model_train_save(model_or_path=None, dirinput='lee_background.cor', d
     elif isinstance(model_or_path, str):
          model_path = model_or_path  ### path  is provided !!!
          model = gensim_model_load(model_path)
+    else :
+         model = model_or_path   ### actual model     
 
-    log("#### Input data building  ")
+    log("#### Input data building  ", model )
     corpus_file = datapath(dirinput)
     to_update = True  if model.wv else False
     model.build_vocab(corpus_file=corpus_file, update=to_update)
@@ -153,12 +160,12 @@ def text_preprocess(sentence, lemmatizer, stop_words):
     import nltk
     sentence = sentence.lower()
     sentence = re.sub(r'[^a-z]', ' ', sentence)   ### ascii only
-    sentence = re.sub(r'\s+', ' ', sentence)  ### Removes all multiple whitespaces with a whitespace in a sentence
-    sentence = [lemmatizer.lemmatize(word) for word in nltk.word_tokenize(sentence) if word not in stop_words]
+    # sentence = re.sub(r'\s+', ' ', sentence)  ### Removes all multiple whitespaces with a whitespace in a sentence
+    # sentence = [lemmatizer.lemmatize(word) for word in nltk.word_tokenize(sentence) if word not in stop_words]
     return ' '.join(sentence)
 
 
-def text_generate_random_sentences( dirout=None, n_sentences=10,):
+def text_generate_random_sentences( dirout=None, n_sentences=5,):
     """
     Generates Random sentences and Preprocesses them
 
@@ -174,8 +181,8 @@ def text_generate_random_sentences( dirout=None, n_sentences=10,):
     lemmatizer = WordNetLemmatizer()
     # stop_words = set(stopwords.words('english'))
     stop_words = []  
-    # sentences = [text_preprocess(gen.sentence(), lemmatizer, stop_words) for i in range(n_sentences)]
-    sentences = [ gen.sentence()  for i in range(n_sentences)]
+    sentences = [text_preprocess(gen.sentence(), lemmatizer, stop_words) for i in range(n_sentences)]
+    # sentences = [ gen.sentence()  for i in range(n_sentences)]
  
     from utilmy import os_makedirs
    
@@ -183,7 +190,7 @@ def text_generate_random_sentences( dirout=None, n_sentences=10,):
         return sentences
     else:
         os_makedirs(dirout)            
-        with open(dirout, mode='a') as fp:
+        with open(dirout, mode='w') as fp:
             for x in sentences:
                 fp.write(x + "\n")
 
@@ -567,3 +574,10 @@ def np_str_to_array(vv, l2_norm=True, mdim=200):
             faiss.normalize_L2(X)  ### Inplace L2 normalization
         log("Normalized X")
         return X
+
+
+
+###################################################################################################
+if __name__ == "__main__":
+    import fire ;
+    fire.Fire()
