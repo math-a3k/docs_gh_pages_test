33dccc3533976e52573628376828c2aaa6daef51
Kev. Noel <arita37@users.noreply.github.com>
2021-10-17 20:02:26 +0900

    Update util_model.py

--- utilmy/nlp/util_model.py
+++ utilmy/nlp/util_model.py
@@ -16,11 +16,132 @@ def help():
     ss = HELP + help_create("utilmy.nlp.util_model")
     print(ss)
 
-
 #################################################################################################
-def text_preprocess(sentence, lemmatizer, stop_words):
+def test_all():
+   test_gensim1()
+   
+def test_gensim1():
+    log("test_gensim")
+    text_generate_random_sentences(dirout='./testdata/mytext1.txt')
+    gensim_model_train_save(dirout='./modelout1/', dirinput='./testdata/mytext1.txt', epochs=1)
+    gensim_model_check('./modelout1/fasttext')
+   
+   
+    model = gensim_model_load('./modelout2')
+    text_generate_random_sentences(dirout='./testdata/mytext2.txt')      
+    gensim_model_train_save(model, dirout='./modelout2/', dirinput='testdata/mytext2.txt', epochs=1)
+    gensim_model_check('./modelout2/fasttext')
+
+
+
+      
+      
+#################################################################################################
+def gensim_model_load(dirin,  modeltype='fastext', **kw):
     """
-    Preprocessing Function
+    Loads the FastText model from the given path
+
+    :param dirin: the path of the saved model
+    :param modeltye:
+    :param kw:
+    :return: loaded model
+    """
+    if modeltype == 'fastext':
+        from gensim.models import FastText
+        loaded_model = FastText.load(f'{dirin}')  ## Full path
+
+    return loaded_model
+
+
+def gensim_model_train_save(model_or_path=None, dirinput='lee_background.cor', dirout="./modelout/model",
+                            epochs=1, pars: dict = None, **kw):
+    """ Trains the Fast text model and saves the model
+      classgensim.models.fasttext.FastText(sentences=None, corpus_file=None, sg=0, hs=0, vector_size=100,
+      alpha=0.025, window=5, min_count=5, max_vocab_size=None, word_ngrams=1, sample=0.001,
+      seed=1, workers=3, min_alpha=0.0001, negative=5, ns_exponent=0.75, cbow_mean=1,
+      hashfxn=<built-in function hash>, epochs=5, null_word=0, min_n=3, max_n=6,
+      sorted_vocab=1, bucket=2000000, trim_rule=None,
+      batch_words=10000, callbacks=(), max_final_vocab=None, shrink_windows=True)
+
+    https://radimrehurek.com/gensim/models/fasttext.html
+
+
+    train(corpus_iterable=None, corpus_file=None, total_examples=None, total_words=None, epochs=None, start_alpha=None,
+          end_alpha=None, word_count=0, queue_factor=2,
+          report_delay=1.0, compute_loss=False, callbacks=(), **kwargs
+
+
+    :param model: The model to train
+    :param dirinput: the filepath of the input data
+    :param dirout: directory to save the model
+    :param fileout: filename of the saving model
+    :epochs: number of epochs to train the model
+    :pars: parameters of the creating FastText
+    :param kw:
+    :return:
+    """
+    from gensim.models import FastText
+    if model_or_path is None:
+        pars = {} if pars is None else pars
+        # model = FastText(vector_size=vector_size, window=window, min_count=min_count)
+        model = FastText(**pars)
+
+    elif isinstance(model_or_path, str):
+         model_path = model_or_path  ### path  is provided !!!
+         model = gensim_model_load(model_path)
+
+    # corpus_file = dirinput
+    corpus_file = datapath(dirinput)
+
+    to_update = True if model.wv else False
+
+    model.build_vocab(corpus_file=corpus_file, update=to_update)
+    nwords = model.corpus_total_words
+
+    log(model.estimate_memory(vocab_size=nwords, report=None))
+    log(nwords, model.get_latest_training_loss())
+
+    model.train(corpus_file=corpus_file, total_words=nwords, epochs=epochs)
+    log(model.get_latest_training_loss())
+    log(model)
+      
+    from utilmy import os_makedirs
+    os_makedirs(dirout)
+    model.save(f'{dirout}')
+
+   
+
+def gensim_model_check(model_path):
+    ''' various model check
+
+       score(sentences, total_sentences=1000000, chunksize=100, queue_factor=2, report_delay=1)
+       Score the log probability for a sequence of sentences. This does not change the fitted model in any way (see train() for that).
+
+       Gensim has currently only implemented score for the hierarchical softmax scheme, so you need to have run word2vec with hs=1 and negative=0 for this to work.
+       Note that you should specify total_sentences; you’ll run into problems if you ask to score more than this number of sentences but it is inefficient to set the value too high.
+
+       Parameters
+       sentences (iterable of list of str) – The sentences iterable can be simply a list of lists of tokens, but for larger corpora, consider an iterable that streams the sentences directly from disk/network. See BrownCorpus, Text8Corpus or LineSentence in word2vec module for such examples.
+       total_sentences (int, optional) – Count of sentences.
+       chunksize (int, optional) – Chunksize of jobs
+       queue_factor (int, optional) – Multiplier for size of queue (number of workers * queue_factor).
+       report_delay (float, optional) – Seconds to wait before reporting progress.
+
+    '''
+    model = gensim_model_load(model_path)
+    print('Log Accuracy:    ', model.wv.evaluate_word_analogies(datapath('questions-words.txt'))[0])
+    print('distance of the word {w1} and {w2} is {d}'.format(w1=model.wv.index_to_key[0],
+                                                                 w2=model.wv.index_to_key[1],
+                                                                 d=model.wv.distance(model.wv.index_to_key[0],
+                                                                                     model.wv.index_to_key[1])))
+
+    print('Most similar words to    ', model.wv.index_to_key[0])
+    print(model.wv.most_similar(model.wv.index_to_key[0]))
+
+
+
+def text_preprocess(sentence, lemmatizer, stop_words):
+    """ Preprocessing Function
     Lowers the characters of a sentence
     Removes all symbols and numbers in a sentence
     Removes all multiple whitespaces with a whitespace in a sentence
@@ -64,120 +185,9 @@ def text_generate_random_sentences(n_sentences=100, dirout=None):
             for x in sentences:
                 fp.write(x + "\n")
 
-
-def gensim_model_load(dirin, filein='fasttext', modeltype='fastext', **kw):
-    """
-    Loads the FastText model from the given path
-
-    :param dirin: the path of the saved model
-    :param modeltye:
-    :param kw:
-    :return: loaded model
-    """
-    if modeltype == 'fastext':
-        from gensim.models import FastText
-        loaded_model = FastText.load(f'{dirin}/{filein}')
-
-    return loaded_model
-
-
-def gensim_model_train_save(model=None, dirinput='lee_background.cor', dirout="./modelout/", fileout='fasttext',
-                            epochs=1, pars: dict = None, **kw):
-    """
-    Trains the Fast text model and saves the model
-      classgensim.models.fasttext.FastText(sentences=None, corpus_file=None, sg=0, hs=0, vector_size=100,
-      alpha=0.025, window=5, min_count=5, max_vocab_size=None, word_ngrams=1, sample=0.001,
-      seed=1, workers=3, min_alpha=0.0001, negative=5, ns_exponent=0.75, cbow_mean=1,
-      hashfxn=<built-in function hash>, epochs=5, null_word=0, min_n=3, max_n=6,
-      sorted_vocab=1, bucket=2000000, trim_rule=None,
-      batch_words=10000, callbacks=(), max_final_vocab=None, shrink_windows=True)
-
-    https://radimrehurek.com/gensim/models/fasttext.html
-
-
-    train(corpus_iterable=None, corpus_file=None, total_examples=None, total_words=None, epochs=None, start_alpha=None,
-          end_alpha=None, word_count=0, queue_factor=2,
-          report_delay=1.0, compute_loss=False, callbacks=(), **kwargs
-
-
-    :param model: The model to train
-    :param dirinput: the filepath of the input data
-    :param dirout: directory to save the model
-    :param fileout: filename of the saving model
-    :epochs: number of epochs to train the model
-    :pars: parameters of the creating FastText
-    :param kw:
-    :return:
-    """
-    from gensim.models import FastText
-    if model is None:
-        pars = {} if pars is None else pars
-        # model = FastText(vector_size=vector_size, window=window, min_count=min_count)
-        model = FastText(**pars)
-
-
-    # elif isinstance(model, str):
-    #     model_path = model  ### path !!!
-    #     model = gensim_model_load(model)
-
-    corpus_file = dirinput
-    if not os.path.exists(dirinput):
-        corpus_file = datapath(dirinput)
-
-    to_update = True if model.wv else False
-
-    model.build_vocab(corpus_file=corpus_file, update=to_update)
-    nwords = model.corpus_total_words
-
-    log(model.estimate_memory(vocab_size=nwords, report=None))
-    log(model.get_latest_training_loss())
-    if to_update:
-        model.train(corpus_file=corpus_file, total_words=nwords, epochs=epochs)
-        log(model.get_latest_training_loss())
-    print(model)
-    from utilmy import os_makedirs
-    os_makedirs(dirout)
-    model.save(f'{dirout}/{fileout}')
-
-
-def gensim_model_check(model_path):
-    ''' various model check
-
-       score(sentences, total_sentences=1000000, chunksize=100, queue_factor=2, report_delay=1)
-       Score the log probability for a sequence of sentences. This does not change the fitted model in any way (see train() for that).
-
-       Gensim has currently only implemented score for the hierarchical softmax scheme, so you need to have run word2vec with hs=1 and negative=0 for this to work.
-       Note that you should specify total_sentences; you’ll run into problems if you ask to score more than this number of sentences but it is inefficient to set the value too high.
-
-       Parameters
-       sentences (iterable of list of str) – The sentences iterable can be simply a list of lists of tokens, but for larger corpora, consider an iterable that streams the sentences directly from disk/network. See BrownCorpus, Text8Corpus or LineSentence in word2vec module for such examples.
-       total_sentences (int, optional) – Count of sentences.
-       chunksize (int, optional) – Chunksize of jobs
-       queue_factor (int, optional) – Multiplier for size of queue (number of workers * queue_factor).
-       report_delay (float, optional) – Seconds to wait before reporting progress.
-
-    '''
-    model = gensim_model_load(model_path)
-    print('Log Accuracy:    ', model.wv.evaluate_word_analogies(datapath('questions-words.txt'))[0])
-    print('The distance of the word {w1} and {w2} is {d}'.format(w1=model.wv.index_to_key[0],
-                                                                 w2=model.wv.index_to_key[1],
-                                                                 d=model.wv.distance(model.wv.index_to_key[0],
-                                                                                     model.wv.index_to_key[1])))
-
-    print('Most similar words to    ', model.wv.index_to_key[0])
-    print(model.wv.most_similar(model.wv.index_to_key[0]))
-
-
-def test_gensim1():
-    text_generate_random_sentences(dirout='./testdata/mytext1.txt')
-    text_generate_random_sentences(dirout='./testdata/mytext2.txt')
-    gensim_model_train_save(dirout='./modelout2/', dirinput='./testdata/mytext1.txt', epochs=10)
-    gensim_model_check('./modelout2/fasttext')
-    model = gensim_model_load('./modelout2')
-    gensim_model_train_save(model, dirout='./modelout2/', dirinput='testdata/mytext2.txt', epochs=10)
-    gensim_model_check('./modelout2/fasttext')
-
-
+   
+   
+   
 
 ####################################################################################################
 ####################################################################################################
