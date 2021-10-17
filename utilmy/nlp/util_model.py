e24001d82ce227bb79ebc05a5357531818cae122
Kev. Noel <arita37@users.noreply.github.com>
2021-10-18 08:33:49 +0900

    Update util_model.py

--- utilmy/nlp/util_model.py
+++ utilmy/nlp/util_model.py
@@ -95,11 +95,11 @@ def gensim_model_train_save(model_or_path=None, dirinput='lee_background.cor', d
 
     log("#### Input data building  ")
     corpus_file = datapath(dirinput)
-    to_update = True # if model.wv else False
+    to_update = True  if model.wv else False
     model.build_vocab(corpus_file=corpus_file, update=to_update)
     nwords = model.corpus_total_words
 
-    log("#### Model training   ")
+    log("#### Model training   ", nwords)
     log('model ram', model.estimate_memory(vocab_size=nwords, report=None))
     log(nwords, model.get_latest_training_loss())
 
