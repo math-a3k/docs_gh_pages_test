120f27f78600ce2d08dfe0e3172f1d68063312b2
arita37 <arita37@users.noreply.github.com>
2021-10-22 14:37:27 +0000

    Auto Docs
    Signed-off-by: GitHub Actions <actions@github.com>

--- docs/flake8_syntax_error.py
+++ docs/flake8_syntax_error.py
@@ -50,3 +50,8 @@ import pyarrow as pa
      ^
 1     E999 IndentationError: expected an indented block
 1
+./utilmy/spark/main.py:13:6: E999 IndentationError: expected an indented block
+import pyarrow as pa
+     ^
+1     E999 IndentationError: expected an indented block
+1
