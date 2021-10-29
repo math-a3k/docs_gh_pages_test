1b997c75b097e2666440cd167c7d5d7da0da7d5e
Kev. Noel <arita37@users.noreply.github.com>
2021-10-29 21:08:07 +0900

    Update vizhtml.py

--- utilmy/viz/vizhtml.py
+++ utilmy/viz/vizhtml.py
@@ -33,7 +33,7 @@ try :
    from pyvis import network as net
 except :
    from utilmy.utilmy import sys_install
-   sys_install(cmd= "pip install python-box python-highcharts matplotlib ipython  mpld3 pandas-highcharts  pretty-html-table  pyvis")
+   sys_install(cmd= "pip install python-box python-highcharts matplotlib ipython  mpld3 pandas-highcharts  pretty-html-table  pyvis  --upgrade-strategy only-if-needed")
    
 
 ############################################################################################
