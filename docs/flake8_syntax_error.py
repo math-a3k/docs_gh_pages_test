9d866c886ca61b350c3f8fe8774273a12809a13a
arita37 <arita37@users.noreply.github.com>
2021-11-10 01:57:26 +0000

    Auto Docs
    Signed-off-by: GitHub Actions <actions@github.com>

--- docs/flake8_syntax_error.py
+++ docs/flake8_syntax_error.py
@@ -49,3 +49,8 @@ form utilmy.utilmy import log, log2
                               ^
 1     E999 SyntaxError: invalid syntax
 1
+./utilmy/util_batch.py:200:31: E999 SyntaxError: invalid syntax
+    def getall(self, date="*" )
+                              ^
+1     E999 SyntaxError: invalid syntax
+1
