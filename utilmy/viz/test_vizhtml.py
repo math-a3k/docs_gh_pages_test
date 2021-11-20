7e64a68e930736c489c3d99d37225141dbd29a20
AlexAdvent <62216338+AlexAdvent@users.noreply.github.com>
2021-11-20 22:35:16 +0530

    Update test_vizhtml.py

--- utilmy/viz/test_vizhtml.py
+++ utilmy/viz/test_vizhtml.py
@@ -1,5 +1,31 @@
-from utilmy.viz.vizhtml import test_getdata,htmlDoc
+from utilmy.viz.vizhtml import htmlDoc
 
+def test_getdata(verbose=True):
+    """data = test_get_data()
+    df   = data['housing.csv']
+    df.head(3)
+    https://github.com/szrlee/Stock-Time-Series-Analysis/tree/master/data
+    """
+    import pandas as pd
+    flist = [
+        'https://raw.githubusercontent.com/samigamer1999/datasets/main/titanic.csv',
+        'https://github.com/subhadipml/California-Housing-Price-Prediction/raw/master/housing.csv',
+        'https://raw.githubusercontent.com/AlexAdvent/high_charts/main/data/stock_data.csv',
+        'https://raw.githubusercontent.com/samigamer1999/datasets/main/cars.csv',
+        'https://raw.githubusercontent.com/samigamer1999/datasets/main/sales.csv',
+        'https://raw.githubusercontent.com/AlexAdvent/high_charts/main/data/weatherdata.csv'
+    ]
+    data = {}
+    for url in flist :
+       fname =  url.split("/")[-1]
+       print( "\n", "\n", url, )
+       df = pd.read_csv(url)
+       data[fname] = df
+       if verbose: print(df)
+       # df.to_csv(fname , index=False)
+    print(data.keys() )
+    return data
+   
 def test_all():
    from utilmy.viz import vizhtml as vi
    log("Visualization ")
