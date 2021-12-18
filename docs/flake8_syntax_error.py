c96318c427585e35f2aa0f399c371a5cd9454ec2
arita37 <arita37@users.noreply.github.com>
2021-12-18 15:24:50 +0000

    Auto Docs
    Signed-off-by: GitHub Actions <actions@github.com>

--- docs/flake8_syntax_error.py
+++ docs/flake8_syntax_error.py
@@ -35,3 +35,12 @@
        ^
 1     E999 SyntaxError: EOL while scanning string literal
 1
+./utilmy/nlp/util_rankmerge.py:32:8: E999 SyntaxError: EOL while scanning string literal
+    """"
+        # evaluate each rank aggregation algorithm by using spearman's rho's and kendall-tau's metrics
+        # for varying levels of ncorrect in the generated rankings    
+    
+    """"
+       ^
+1     E999 SyntaxError: EOL while scanning string literal
+1
