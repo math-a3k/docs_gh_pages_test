270016ad384b009c94ddb8cb8da3cb5cd6ad6148
Charis Chrisna <62300057+notpestilence@users.noreply.github.com>
2022-01-20 23:57:36 +0700

    Update util_similarity.py
    Added docstrings to similarity functions

--- utilmy/deeplearning/keras/util_similarity.py
+++ utilmy/deeplearning/keras/util_similarity.py
@@ -16,14 +16,17 @@ def help():
     print(ss)
 
 
-
-
 ######################################################################################
 def test_all():
     test_tf_cdist()
+# Charis: What does this test_all() do? 
+# Why can't we directly call test_tf_cdist() on `test.py?`
 
 
 def test_tf_cdist():
+    """
+    Tests all similarity functions.
+    """
     from scipy.spatial.distance import cdist
     from tqdm import tqdm
     from utilmy.deeplearning.keras.util_similarity import tf_cdist
@@ -63,6 +66,15 @@ def test_tf_cdist():
 
 ######################################################################################
 def tf_cdist(left, right, metric='euclidean'):
+    """
+    Test unit to calculate distance based on `metric`.
+    Returns calculated `metric` distance between point A and B.
+
+    Arguments: 
+    `left` - Point A coordinates;
+    `right` - Point B coordinates;
+    `metric` - 'euclidean' or 'cosine'
+    """
     #### distance between tensor
     if metric == 'euclidean':
         return tf_cdist_euclidean(left, right)
@@ -74,6 +86,13 @@ def tf_cdist(left, right, metric='euclidean'):
 
 
 def tf_cdist_euclidean(left, right):
+    """
+    Wrapper function to return euclidean distance from Point A to B.
+
+    Arguments:
+    `left` - Point A coordinates;
+    `right` - Point B coordinates
+    """
     left, right                       = __cast_left_and_right_to_tensors(left, right)
     rows_count_left, rows_count_right = __get_rows_counts(left, right)
     left_sqr                          = __get_tensor_sqr(left, (-1, 1), (1, rows_count_right))
@@ -85,6 +104,13 @@ def tf_cdist_euclidean(left, right):
     return distance
 
 def tf_cdist_cos(left, right):
+    """
+    Wrapper function to return cosine distance from Point A to B.
+
+    Arguments:
+    `left` - Point A coordinates;
+    `right` - Point B coordinates
+    """
     left, right = __cast_left_and_right_to_tensors(left, right)
     norm_left   = __get_tensor_reshaped_norm(left, (-1, 1))
     norm_right  = __get_tensor_reshaped_norm(right, (1, -1))
@@ -93,21 +119,38 @@ def tf_cdist_cos(left, right):
     distance    = tf.cast(distance, tf.float32)
     return distance
 
-
-
 def __cast_left_and_right_to_tensors(left, right):
+    """
+    Convert coordinates into `float32` `Tensor` types.
+    
+    Arguments:
+    `left` - Point A coordinates, any type;
+    `right` - Point B coordinates, any type
+    """
     left  = tf.cast(tf.convert_to_tensor(left), dtype=tf.float32)
     right = tf.cast(tf.convert_to_tensor(right), dtype=tf.float32)
     return left, right
 
-
 def __get_rows_counts(left, right):
-    count_left  = tf.shape(left)[0]
-    count_right = tf.shape(right)[0]
+    """
+    Returns each `Tensor` row counts.
+    
+    Arguments:
+    `left` - Point A coordinates of type `Tensor`;
+    `right` - Point B coordinates of type `Tensor`
+    """
+    count_left, count_right  = tf.shape(left)[0], tf.shape(right)[0]
     return count_left, count_right
 
-
 def __get_tensor_sqr(tensor, reshape_shape, tile_shape):
+    """
+    Returns squared, tiled `Tensor`
+
+    Arguments:
+    `tensor` - `Tensor` object; 
+    `reshape_shape` - Tuple of length 2;
+    `tile_shape` - 1-D Array
+    """
     sqr = tf.pow(tensor, 2.0)
     sqr = tf.reduce_sum(sqr, axis=1)
     sqr = tf.reshape(sqr, reshape_shape)
@@ -115,9 +158,14 @@ def __get_tensor_sqr(tensor, reshape_shape, tile_shape):
     return sqr
 
 
-
-
 def __get_tensor_reshaped_norm(tensor, reshape_shape):
+    """
+    Returns a normalized `Tensor`
+
+    Arguments:
+    `tensor` - `Tensor` object; 
+    `reshape_shape` - Tuple of length 2
+    """
     norm = tf.norm(tensor, axis=1)
     norm = tf.reshape(norm, reshape_shape)
     return norm
