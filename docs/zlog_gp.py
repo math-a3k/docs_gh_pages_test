08c5de0bd1caba403ba0de3286d1585c32dc2490
arita37 <arita37@users.noreply.github.com>
2022-02-20 14:26:47 +0000

    Auto Docs
    Signed-off-by: GitHub Actions <actions@github.com>

---
+++ docs/zlog_gp.py
@@ -0,0 +1,19 @@
+I/O count: 2/1
+Grid shape: 18x1
+Cells: ['+', '-', '*', '/']
+o2i: No
+Nest size: 20
+Parasitic Probability: 0.3
+kmax: 100000
+rand_inject: 25
+Step size: Lévy flight
+print_after: 100
+print_best: 0
+
+
+['INPUT(n0)\n', 'INPUT(n1)\n', 'OUTPUT(n4)\n', 'x4 = (0)\n']
+
+
+
+
+DONE
