5c7f868465446d12dbcb43dfda28e862175941a2
Kev. Noel <arita37@users.noreply.github.com>
2022-02-25 00:25:46 +0900

    adataset

--- utilmy/adatasets.py
+++ utilmy/adatasets.py
@@ -1,5 +1,9 @@
-# pylint: disable=C0321,C0103,C0301,E1305,E1121,C0302,C0330,C0111,W0613,W0611,R1705
 # -*- coding: utf-8 -*-
+MNAME = "utilmy.adatasets"
+HELP = """ utils for dataset donwloading
+
+
+"""
 import os, sys, time, datetime,inspect, json, pandas as pd, numpy as np
 from pathlib import Path
 from sklearn.datasets import load_boston
@@ -8,8 +12,15 @@ from scipy.io.arff import loadarf
 
 
 
-from utilmy import (os_makedirs, os_system, global_verbosity, git_current_hash, git_repo_root
-                           )
+#############################################################################################
+from utilmy import (os_makedirs, os_system, global_verbosity,  git_repo_root )
+from utilmy import log, log2
+
+def help():
+    from utilmy import help_create
+    print( HELP + help_create(MNAME) )
+
+
 
 
 ##############################################################################################
@@ -38,8 +49,6 @@ def test_all():
     test()
     test_pd_utils()
 
-
-
 def test0():
     log("Testing  ...")
     test_dataset_regression_fake(nrows=500, n_features=17)
@@ -59,19 +68,9 @@ def test1():
     '''
     pd_train_test_split2(df, "block")
 
-    
-####################################################################################################
-verbosity = 3
-
-def log(*s):
-    if verbosity>= 1: print(*s, flush=True)
-
-def log2(*s):
-    if verbosity>= 1: print(*s, flush=True)
-
 
 ####################################################################################################
-def dataset_classifier_XXXXX(nrows=500, **kw):
+def atemplate_dataset_classifier_XXXXX(nrows=500, **kw):
     """
 
     """
@@ -81,30 +80,6 @@ def dataset_classifier_XXXXX(nrows=500, **kw):
     df = pd.DataFrame
     pars = { 'colnum': colnum, 'colcat': colcat, "coly": coly, 'info': '' }
     return df, pars  
-
-
-
-
-####################################################################################################
-def pd_train_test_split(df, coly=None):
-    from sklearn.model_selection import train_test_split
-    X,y = df.drop(coly,axis=1), df[[coly]]
-    X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.05, random_state=2021)
-    X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021)
-    return X_train, X_valid, y_train, y_valid, X_test, y_test
-
-
-
-def pd_train_test_split2(df, coly):
-    from sklearn.model_selection import train_test_split
-    log2(df.dtypes)
-    X,y = df.drop(coly,  axis=1), df[coly]
-    log2('y', np.sum(y[y==1]) , X.head(3))
-    X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.05, random_state=2021)
-    X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021)
-    num_classes                                = len(set(y_train_full.values.ravel()))
-    return X,y, X_train, X_valid, y_train, y_valid, X_test,  y_test, num_classes
-
 
 
 ####################################################################################################
@@ -252,71 +227,90 @@ def test_dataset_regression_boston_traintest():
 
 
 
-
 ###################################################################################################
-def fetch_dataset(url_dataset, path_target=None, file_target=None):
-    """Fetch dataset from a given URL and save it.
+if 'utils':
+    def fetch_dataset(url_dataset, path_target=None, file_target=None):
+        """Fetch dataset from a given URL and save it.
 
-    Currently `github`, `gdrive` and `dropbox` are the only supported sources of
-    data. Also only zip files are supported.
+        Currently `github`, `gdrive` and `dropbox` are the only supported sources of
+        data. Also only zip files are supported.
 
-    :param url_dataset:   URL to send
-    :param path_target:   Path to save dataset
-    :param file_target:   File to save dataset
+        :param url_dataset:   URL to send
+        :param path_target:   Path to save dataset
+        :param file_target:   File to save dataset
 
-    """
-    log("###### Download ##################################################")
-    from tempfile import mktemp, mkdtemp
-    from urllib.parse import urlparse, parse_qs
-    import pathlib
-    fallback_name        = "features"
-    download_path        = path_target
-    supported_extensions = [ ".zip" ]
-
-    if path_target is None:
-        path_target   = mkdtemp(dir=os.path.curdir)
-        download_path = path_target
-    else:
-        pathlib.Path(path_target).mkdir(parents=True, exist_ok=True)
-
-    if file_target is None:
-        file_target = fallback_name # mktemp(dir="")
-
-
-    if "github.com" in url_dataset:
         """
-             # https://github.com/arita37/dsa2_data/raw/main/input/titanic/train/features.zip
-              https://github.com/arita37/dsa2_data/raw/main/input/titanic/train/features.zip            
-              https://raw.githubusercontent.com/arita37/dsa2_data/main/input/titanic/train/features.csv            
-              https://raw.githubusercontent.com/arita37/dsa2_data/tree/main/input/titanic/train/features.zip             
-              https://github.com/arita37/dsa2_data/blob/main/input/titanic/train/features.zip
-                 
-        """
-        # urlx = url_dataset.replace(  "github.com", "raw.githubusercontent.com" )
-        urlx = url_dataset.replace("/blob/", "/raw/")
-        urlx = urlx.replace("/tree/", "/raw/")
-        log(urlx)
+        log("###### Download ##################################################")
+        from tempfile import mktemp, mkdtemp
+        from urllib.parse import urlparse, parse_qs
+        import pathlib
+        fallback_name        = "features"
+        download_path        = path_target
+        supported_extensions = [ ".zip" ]
 
-        urlpath = urlx.replace("https://github.com/", "github_")
-        urlpath = urlpath.split("/")
-        fname = urlpath[-1]  ## filaneme
-        fpath = "-".join(urlpath[:-1])[:-1]   ### prefix path normalized
-        assert "." in fname, f"No filename in the url {urlx}"
+        if path_target is None:
+            path_target   = mkdtemp(dir=os.path.curdir)
+            download_path = path_target
+        else:
+            pathlib.Path(path_target).mkdir(parents=True, exist_ok=True)
 
-        os.makedirs(download_path + "/" + fpath, exist_ok= True)
-        full_filename = os.path.abspath( download_path + "/" + fpath + "/" + fname )
-        log('#### Download saving in ', full_filename)
+        if file_target is None:
+            file_target = fallback_name # mktemp(dir="")
 
-        import requests
-        with requests.Session() as s:
-            res = s.get(urlx)
-            if res.ok:
-                print(res.ok)
-                with open(full_filename, "wb") as f:
-                    f.write(res.content)
-            else:
-                raise res.raise_for_status()
-        return full_filename
+
+        if "github.com" in url_dataset:
+            """
+                # https://github.com/arita37/dsa2_data/raw/main/input/titanic/train/features.zip
+                https://github.com/arita37/dsa2_data/raw/main/input/titanic/train/features.zip            
+                https://raw.githubusercontent.com/arita37/dsa2_data/main/input/titanic/train/features.csv            
+                https://raw.githubusercontent.com/arita37/dsa2_data/tree/main/input/titanic/train/features.zip             
+                https://github.com/arita37/dsa2_data/blob/main/input/titanic/train/features.zip
+                    
+            """
+            # urlx = url_dataset.replace(  "github.com", "raw.githubusercontent.com" )
+            urlx = url_dataset.replace("/blob/", "/raw/")
+            urlx = urlx.replace("/tree/", "/raw/")
+            log(urlx)
+
+            urlpath = urlx.replace("https://github.com/", "github_")
+            urlpath = urlpath.split("/")
+            fname = urlpath[-1]  ## filaneme
+            fpath = "-".join(urlpath[:-1])[:-1]   ### prefix path normalized
+            assert "." in fname, f"No filename in the url {urlx}"
+
+            os.makedirs(download_path + "/" + fpath, exist_ok= True)
+            full_filename = os.path.abspath( download_path + "/" + fpath + "/" + fname )
+            log('#### Download saving in ', full_filename)
+
+            import requests
+            with requests.Session() as s:
+                res = s.get(urlx)
+                if res.ok:
+                    print(res.ok)
+                    with open(full_filename, "wb") as f:
+                        f.write(res.content)
+                else:
+                    raise res.raise_for_status()
+            return full_filename
+
+
+    def pd_train_test_split(df, coly=None):
+        from sklearn.model_selection import train_test_split
+        X,y = df.drop(coly,axis=1), df[[coly]]
+        X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.05, random_state=2021)
+        X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021)
+        return X_train, X_valid, y_train, y_valid, X_test, y_test
+
+
+    def pd_train_test_split2(df, coly):
+        from sklearn.model_selection import train_test_split
+        log2(df.dtypes)
+        X,y = df.drop(coly,  axis=1), df[coly]
+        log2('y', np.sum(y[y==1]) , X.head(3))
+        X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.05, random_state=2021)
+        X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021)
+        num_classes                                = len(set(y_train_full.values.ravel()))
+        return X,y, X_train, X_valid, y_train, y_valid, X_test,  y_test, num_classes
 
 
 
