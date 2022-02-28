006524941984c87ad7d099eff0b89210b0de24f4
Kev. Noel <arita37@users.noreply.github.com>
2022-02-28 10:47:20 +0900

    ok

--- init.sh
+++ init.sh
@@ -15,3 +15,23 @@ alias aa='PS1="[\W]\\$ "'
 
 alias ipy="ipython --no-autoindent"
 
+
+
+################################################################################################
+function git_autocommit {
+   now
+   git  add -A
+   git  commit -m "auto commit"
+}
+
+
+
+
+################################################################################################
+#### Auto Batch
+while true; do git_autocommit; sleep 3600; done
+
+
+
+#The $0 variable is reserved for the function’s name.
+#The $# variable holds the number of positional parameters/arguments passed to the function.
