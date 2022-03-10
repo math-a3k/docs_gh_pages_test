99a72966cec7ea65d9c98d8239175d0ae56ea08e
Kev. Noel <arita37@users.noreply.github.com>
2022-03-10 15:36:05 +0900

    ok

--- docs_ghpage/conf.py
+++ docs_ghpage/conf.py
@@ -91,6 +91,25 @@ html_theme = 'sphinx_rtd_theme'
 html_show_sourcelink = False
 
 
+
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
